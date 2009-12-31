829624989b614f3efc7a130068473b0d3cb41db7
John Florian <john_florian@dart.biz>
2009-12-31 11:49:49 -0500

    New - Bacula administrator module
    This module configures a host with the tray monitor and the GUI console.

---
+++ modules/bacula_admin/manifests/init.pp
@@ -0,0 +1,29 @@
+# /etc/puppet/modules/bacula_admin/manifests/init.pp
+
+class bacula_admin {
+
+    package { "bacula-traymonitor":
+	ensure	=> installed,
+    }
+
+    package { "bacula-console-bat":
+	ensure	=> installed,
+    }
+
+    file { "/etc/tray-monitor.conf":
+        group	=> "root",
+        mode    => 644,
+        owner   => "root",
+        require => Package["bacula-traymonitor"],
+        source  => "puppet:///bacula_admin/tray-monitor.conf",
+    }
+
+    file { "/etc/bat.conf":
+        group	=> "root",
+        mode    => 640,
+        owner   => "root",
+        require => Package["bacula-console-bat"],
+        source  => "puppet:///bacula_admin/bat.conf",
+    }
+
+}
