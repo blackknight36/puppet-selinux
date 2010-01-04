e5b7e99a4c97c67bd00929a532867f9f3b41c33e
John Florian <john_florian@dart.biz>
2010-01-04 16:32:30 -0500

    New - autofs support for CK's workstation

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -37,6 +37,7 @@ class autofs {
         owner   => "root",
         source	=> $hostname ? {
             "mdct-dev12"        => "puppet:///autofs/auto.mnt-mdct-dev12",
+            "mdct-dev6"         => "puppet:///autofs/auto.mnt-mdct-dev6",
             default             => "puppet:///autofs/auto.mnt-local",
         },
     }
