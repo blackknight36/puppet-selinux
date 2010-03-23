fda4362e9cbbd8771a16f780c72a745508a6c77e
John Florian <john_florian@dart.biz>
2010-03-23 11:46:17 -0400

    Fix - Notes Client assumes /pub is mounted
    While it may be configured to be mounted in autofs, there's no guarantee to
    where mdct-00fs is exporting it.  Using http is more reliable and cleaner.

--- modules/lotus_notes_client/manifests/init.pp
+++ modules/lotus_notes_client/manifests/init.pp
@@ -2,14 +2,11 @@
 
 class lotus_notes_client {
 
-    include autofs
+    $key_store="http://mdct-00fs.dartcontainer.com/ftp/pub/fedora/mdct/signing_keys"
 
     exec { "import_ibm_signing_key":
-        command => "rpm --import /pub/fedora/mdct/signing_keys/pub_ibm_lotus_notes.gpg",
+        command => "rpm --import $key_store/pub_ibm_lotus_notes.gpg",
         unless  => "rpm -q gpg-pubkey-34f9ae75",
-        require => [
-            File["/pub"],       # from autofs
-        ],
     }
 
     package { "ibm_lotus_notes":
@@ -25,14 +22,11 @@ class lotus_notes_client {
     # it the key is trusted for installing packages signed with it, hence the
     # selector mess.
     exec { "import_f8_new_signing_key":
-        command => "rpm --import /pub/fedora/mdct/signing_keys/RPM-GPG-KEY-fedora-8-new",
+        command => "rpm --import $key_store/RPM-GPG-KEY-fedora-8-new",
         unless  => $hostname ? {
             "mdct-dev12"        => "true",
             default             => "rpm -q gpg-pubkey-4f2a6fd2",
         },
-        require => [
-            File["/pub"],       # from autofs
-        ],
     }
 
     # This package brought forth from Fedora 8.  Notes will run without these
