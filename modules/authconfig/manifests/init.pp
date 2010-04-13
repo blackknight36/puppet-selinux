2ef8e69bc970fe54107122e0631389d32c072ad6
John Florian <john_florian@dart.biz>
2010-04-13 16:40:18 -0400

    Fix - authconfig requires nss_ldap
    Our authentication setup requires the nss_ldap package to function properly,
    so require it.

--- modules/authconfig/manifests/init.pp
+++ modules/authconfig/manifests/init.pp
@@ -6,11 +6,16 @@ class authconfig {
 	 ensure => installed
     }
 
+    package { "nss_ldap":
+	 ensure => installed
+    }
+
     exec { "authconfig":
         #command => 'authconfig --enableldap --enableldapauth --ldapserver="ldap://10.1.192.106,ldap://mdct-01gw,ldap://mdct-00gw" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --update',
         command => 'authconfig --enableldap --enableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --update',
 	require => [
             Package["authconfig"],
+            Package["nss_ldap"],
         ],
     	unless => "grep -q USELDAPAUTH=yes /etc/sysconfig/authconfig"
     }
