4446b073ad4bae90ae0665868f37c8845dbb7940
John Florian <john_florian@dart.biz>
2010-06-07 13:58:20 -0400

    New - sudo support for Fedora 13
    This is now very clean since sudo now provides a sudoers.d directory -- yeah!

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -6,14 +6,35 @@ class sudo {
 	ensure	=> installed
     }
 
-    file { "/etc/sudoers":
-        group	=> "root",
-        mode    => 440,
-        owner   => "root",
-        require => Package["sudo"],
-        source  => [
-            "puppet:///sudo/sudoers.$hostname",
-            "puppet:///sudo/sudoers",
-        ],
+    if $operatingsystem == "Fedora" {
+
+        if $operatingsystemrelease < 13 {
+
+            file { "/etc/sudoers":
+                group	=> "root",
+                mode    => 440,
+                owner   => "root",
+                require => Package["sudo"],
+                source  => [
+                    "puppet:///sudo/sudoers.$hostname",
+                    "puppet:///sudo/sudoers.preF13",
+                ],
+            }
+
+        } else {
+
+            file { "/etc/sudoers.d/mdct":
+                group	=> "root",
+                mode    => 440,
+                owner   => "root",
+                require => Package["sudo"],
+                source  => [
+                    "puppet:///sudo/sudoers.$hostname",
+                    "puppet:///sudo/sudoers.mdct",
+                ],
+            }
+
+        }
+
     }
 }
