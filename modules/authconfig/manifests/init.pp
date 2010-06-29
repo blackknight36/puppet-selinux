987d0b1e40fd9d4534ba89d09142144155698717
John Florian <john_florian@dart.biz>
2010-06-29 08:29:23 -0400

    Fix - authconfig for Fedora 13 unreliable
    For minimal F13 installs the prior authconfig worked just fine.  However,
    fuller desktop installs had auth problems because these installs brought in
    SSSD.  This revision configures all types of installs to use SSSD and
    configures it so that identity is via LDAP and authorization is via Kerberos
    against Dart's Active Directory services.  LDAP authorization had to be
    abandoned because SSSD refuses to to do unencrypted LDAP auth and MDCT's LDAP
    server presently does not offer encryption.

--- modules/authconfig/manifests/init.pp
+++ modules/authconfig/manifests/init.pp
@@ -3,39 +3,15 @@
 class authconfig {
 
     package { "authconfig":
-	 ensure => installed
-    }
-
-    package { "nss_ldap":
-	 ensure => installed
+         ensure => installed
     }
 
     package { "pam":
         ensure  => installed,
     }
 
-    package { "nscd":
-	ensure	=> installed,
-    }
-
-    service { "nscd":
-        enable		=> true,
-        ensure		=> running,
-        hasrestart	=> true,
-        hasstatus	=> true,
-        require		=> [
-            Package["nscd"],
-        ],
-    }
-
-    if $operatingsystem == "Fedora" and $operatingsystemrelease >= 13 {
-        exec { "allow-use-nfs-home-dirs":
-            command => "setsebool -P use_nfs_home_dirs on",
-            unless  => "getsebool use_nfs_home_dirs | grep -q -- 'on$'",
-        }
-    }
-
     file { "/etc/pam.d/system-auth-ac":
+        before  => Exec["authconfig"],
         group   => "root",
         mode    => 644,
         owner   => "root",
@@ -46,15 +22,112 @@ class authconfig {
         ],
     }
 
-    exec { "authconfig":
-        #command => 'authconfig --enableldap --enableldapauth --ldapserver="ldap://10.1.192.106,ldap://mdct-01gw,ldap://mdct-00gw" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --update-all',
-        command => 'authconfig --enableldap --enableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --updateall',
-	require => [
-            File["/etc/pam.d/system-auth-ac"],
-            Package["authconfig"],
-            Package["nss_ldap"],
-        ],
-    	unless => "grep -q USELDAPAUTH=yes /etc/sysconfig/authconfig"
+    if $operatingsystem == "Fedora" {
+        if $operatingsystemrelease < 13 {
+
+            package { "nss_ldap":
+                ensure  => installed
+            }
+
+            package { "nscd":
+                ensure  => installed,
+                require => Package["nss_ldap"],
+            }
+
+            service { "nscd":
+                enable          => true,
+                ensure          => running,
+                hasrestart      => true,
+                hasstatus       => true,
+                require         => Package["nscd"],
+            }
+
+            exec { "authconfig":
+                command => 'authconfig --enableldap --enableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --updateall',
+                require => [
+                    File["/etc/pam.d/system-auth-ac"],
+                    Package["authconfig"],
+                    Service["nscd"],
+                ],
+                unless => "grep -q USELDAPAUTH=yes /etc/sysconfig/authconfig"
+            }
+
+        } else { # this is Fedora 13 or later
+
+            # As of Fedora 13, the policy is more strict and this is required
+            # for logins to gain access to the user's home dir, if it is on
+            # NFS.  Interestingly, this only affects the cd within login as it
+            # is still possible to enter the home dir after login when this is
+            # off.
+            selboolean { "use_nfs_home_dirs":
+                persistent      => true,
+                value           => on,
+            }
+
+            package { "krb5-libs":
+                ensure  => installed,
+            }
+
+            # As of Fedora 13, nscd (and it's dependent nss_ldap) is being
+            # replaced by sssd, but oddly the packaging doesn't obsolete it,
+            # so we do so here.
+            package { "nss_ldap":
+                ensure  => absent,
+            }
+
+            package { "nscd":
+                ensure  => absent,
+                require => Package["nss_ldap"],
+            }
+
+            package { "sssd":
+                ensure  => installed,
+                require => Package["nscd"],
+            }
+
+            # As of Fedora 13, encryption is required to a directory server if
+            # it is to be used for authentication.  Since MDCT's LDAP server
+            # doesn't provide encryption, authentication is configured for
+            # Kerberos against the Active Directory while all other user
+            # account information continues to be obtained from LDAP.
+            exec { "authconfig":
+                before  => File["/etc/sssd/sssd.conf"],
+                command => 'authconfig --enableldap --disableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --enablesssd --disablesssdauth --enablekrb5 --krb5realm=DARTCONTAINER.COM --krb5kdc=dartcontainer.com --enablecachecreds --updateall',
+                require => [
+                    File["/etc/pam.d/system-auth-ac"],
+                    Package["authconfig"],
+                    Package["nscd"],
+                    Package["sssd"],
+                ],
+                unless => "grep -q USEKERBEROS=yes /etc/sysconfig/authconfig"
+            }
+
+            # According to F13's authconfig(8), authconfig does NOT configure
+            # sssd.conf (must be done manually), so one is provided here.
+            file { "/etc/sssd/sssd.conf":
+                group   => "root",
+                mode    => "0600",
+                owner   => "root",
+                require => Package["sssd"],
+                source  => "puppet:///authconfig/sssd.conf",
+            }
+
+            service { "sssd":
+                enable          => true,
+                ensure          => running,
+                hasrestart      => true,
+                hasstatus       => true,
+                require         => [
+                    Exec["authconfig"],
+                    Package["sssd"],
+                ],
+                subscribe       => [
+                    File["/etc/sssd/sssd.conf"],
+                ],
+            }
+
+        }
+
     }
 
 }
