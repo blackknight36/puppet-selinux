cb1d92c12317af202fdc04edd12e2b53ec3e09ce
John Florian <john_florian@dart.biz>
2010-07-01 17:07:44 -0400

    Change - mdct-dev12 now running Fedora 13
    Minor changes needed for the transition.

--- modules/lotus_notes_client/manifests/init.pp
+++ modules/lotus_notes_client/manifests/init.pp
@@ -25,17 +25,9 @@ class lotus_notes_client {
         ],
     }
 
-    # mdct-dev12 treated specially.  I manually imported the key (via rpm
-    # --import) and then removed it (via rpm -e) to verify this class would
-    # work correctly, but the key always fails to import there now.  However,
-    # it the key is trusted for installing packages signed with it, hence the
-    # selector mess.
     exec { "import_f8_new_signing_key":
         command => "rpm --import $key_store/RPM-GPG-KEY-fedora-8-new",
-        unless  => $hostname ? {
-            "mdct-dev12"        => "true",
-            default             => "rpm -q gpg-pubkey-4f2a6fd2",
-        },
+        unless  => "rpm -q gpg-pubkey-4f2a6fd2",
     }
 
     # This package brought forth from Fedora 8.  Notes will run without these
