8babe34dcefbbf9c70fad4c9e3e6dbc510cd9369
John Florian <john_florian@dart.biz>
2010-07-01 14:19:50 -0400

    Fix - unwanted-service pcscd only for F10?
    Not sure why this was disabled only for F10.  Maybe a range would be more
    apropos, but pcscd has been around for a long time.  Hmmm....

--- modules/unwanted-services/manifests/init.pp
+++ modules/unwanted-services/manifests/init.pp
@@ -3,63 +3,59 @@
 class unwanted-services {
 
     service { "avahi-daemon":
-        enable		=> false,
-        ensure		=> stopped,
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        ensure          => stopped,
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
     service { "bluetooth":
-        enable		=> false,
-        ensure		=> stopped,
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        ensure          => stopped,
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
     service { "firstboot":
-        enable		=> false,
-        ensure		=> stopped,
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        ensure          => stopped,
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
     service { "iscsi":
-        enable		=> false,
-        ensure		=> stopped,
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        ensure          => stopped,
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
     service { "iscsid":
-        enable		=> false,
-        ensure		=> stopped,
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        ensure          => stopped,
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
     service { "livesys":
-        enable		=> false,
-        #ensure		=> stopped,     # there is nothing to stop
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        #ensure         => stopped,     # there is nothing to stop
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
     service { "livesys-late":
-        enable		=> false,
-        #ensure		=> stopped,     # there is nothing to stop
-        hasrestart	=> true,
-        hasstatus	=> true,
+        enable          => false,
+        #ensure         => stopped,     # there is nothing to stop
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
-    if $operatingsystemrelease == "10" {
-        
-        service { "pcscd":
-            enable	=> false,
-            ensure	=> stopped,
-            hasrestart	=> true,
-            hasstatus	=> true,
-        }
-
+    service { "pcscd":
+        enable          => false,
+        ensure          => stopped,
+        hasrestart      => true,
+        hasstatus       => true,
     }
 
 }
