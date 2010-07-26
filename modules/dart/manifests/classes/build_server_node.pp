971437cc4308fdd450ca8185b0d9a6ad7de7aabb
John Florian <john_florian@dart.biz>
2010-07-26 14:48:57 -0400

    Janitorial - add comments to builder class re TODO

--- modules/dart/manifests/classes/build_server_node.pp
+++ modules/dart/manifests/classes/build_server_node.pp
@@ -4,4 +4,7 @@ class dart::build_server_node inherits dart::server_node {
     include packages::developer
     include packages::net_tools
     include unwanted-services
+
+    # TODO: create /j/{git,rpmbuild}
+    # Right now it easiest to just rsync /j from another build server.
 }
