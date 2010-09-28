d1130f7b89326eea9c91ee99dc83502079a53e68
John Florian <john_florian@dart.biz>
2010-09-28 14:58:31 -0400

    New - plymouth module

---
+++ modules/plymouth/manifests/init.pp
@@ -0,0 +1,22 @@
+# /etc/puppet/modules/plymouth/manifests/init.pp
+
+class plymouth {
+
+    package { "plymouth":
+        ensure  => installed,
+    }
+
+    package { "plymouth-scripts":
+        ensure  => installed,
+    }
+
+    # Only configure the default plymouth theme if one has been specified.
+    if "$plymouth_default_theme" != "" {
+        exec { "set-plymouth-theme":
+            # command returns 1 even when successful
+            command => "plymouth-set-default-theme $plymouth_default_theme || true",
+            unless  => "plymouth-set-default-theme | grep -q $plymouth_default_theme",
+        }
+    }
+
+}
