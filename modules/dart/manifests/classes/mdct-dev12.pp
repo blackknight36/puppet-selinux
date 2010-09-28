7a61d4e1530b443380747965e91a945bcfbca178
John Florian <john_florian@dart.biz>
2010-09-28 14:58:55 -0400

    Change - mdct-dev12 uses plymouth details theme

--- modules/dart/manifests/classes/mdct-dev12.pp
+++ modules/dart/manifests/classes/mdct-dev12.pp
@@ -1,6 +1,9 @@
 # /etc/puppet/modules/dart/manifests/classes/mdct-dev12.pp
 
 class dart::mdct-dev12 inherits dart::workstation_node {
+    $plymouth_default_theme = "details"
+    include plymouth
+
     # passwords generated with bacula-password-generator package
     $bacula_client_director_password = "2hNcW1n2jkNU5ywm4TK6CrY2yDmqlEPcr2SoRP0abEHW"
     $bacula_client_director_monitor_password = "WqQvFNJdbiIfyxnKkoocVQFcNgY0CLVcKXok1TtrhJTH"
