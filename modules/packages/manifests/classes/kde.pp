587e4c5de79676895b0d6984b9c402fe14aae643
John Florian <john_florian@dart.biz>
2010-12-02 11:03:11 -0500

    Fix - add missing packages for KDE Software Compilation
    The packages::kde class should now be roughly equivalent to installing
    the yum group "KDE Software Compilation", at least for Fedora 14.

--- modules/packages/manifests/classes/kde.pp
+++ modules/packages/manifests/classes/kde.pp
@@ -3,35 +3,171 @@
 class packages::kde {
 
     package { "amarok":
-	ensure	=> installed,
+        ensure  => installed,
+    }
+
+    package { "cups-pk-helper":
+        ensure  => installed,
+    }
+
+    package { "digikam":
+        ensure  => installed,
     }
 
     package { "k3b":
-	ensure	=> installed,
+        ensure  => installed,
     }
 
     package { "k3b-extras-freeworld":
-	ensure	=> installed,
+        ensure  => installed,
     }
 
-    package { "kdeartwork":
-	ensure	=> installed,
+    package { "kaffeine":
+        ensure  => installed,
+    }
+
+    package { "kbluetooth":
+        ensure  => installed,
+    }
+
+    package { "kcm-gtk":
+        ensure  => installed,
+    }
+
+    package { "kcm_touchpad":
+        ensure  => installed,
+    }
+
+    package { "kde-plasma-networkmanagement":
+        ensure  => installed,
     }
 
     package { "kde-plasma-yawp":
-	ensure	=> installed,
+        ensure  => installed,
+    }
+
+    package { "kde-settings-pulseaudio":
+        ensure  => installed,
+    }
+
+    package { "kdeaccessibility":
+        ensure  => installed,
+    }
+
+    package { "kdeartwork":
+        ensure  => installed,
+    }
+
+    package { "kdeartwork-screensavers":
+        ensure  => installed,
+    }
+
+    package { "kdebase":
+        ensure  => installed,
+    }
+
+    package { "kdebase-workspace-python-applet":
+        ensure  => installed,
+    }
+
+    package { "kdegames":
+        ensure  => installed,
+    }
+
+    package { "kdegraphics":
+        ensure  => installed,
+    }
+
+    package { "kdemultimedia":
+        ensure  => installed,
+    }
+
+    package { "kdenetwork":
+        ensure  => installed,
+    }
+
+    package { "kdepim":
+        ensure  => installed,
+    }
+
+    package { "kdeplasma-addons":
+        ensure  => installed,
+    }
+
+    package { "kdeutils":
+        ensure  => installed,
+    }
+
+    package { "kdeutils-printer-applet":
+        ensure  => installed,
+    }
+
+    package { "kdm":
+        ensure  => installed,
+    }
+
+    package { "kipi-plugins":
+        ensure  => installed,
     }
 
     package { "kmplayer":
-	ensure	=> installed,
+        ensure  => installed,
     }
 
     package { "koffice-krita":
-	ensure	=> installed,
+        ensure  => installed,
+    }
+
+    package { "konq-plugins":
+        ensure  => installed,
+    }
+
+    package { "konversation":
+        ensure  => installed,
+    }
+
+    package { "kpackagekit":
+        ensure  => installed,
+    }
+
+    package { "ksshaskpass":
+        ensure  => installed,
+    }
+
+    package { "ktorrent":
+        ensure  => installed,
     }
 
     package { "phonon-backend-gstreamer":
-	ensure	=> installed,
+        ensure  => installed,
+    }
+
+    package { "pinentry-qt":
+        ensure  => installed,
+    }
+
+    package { "qtcurve-gtk2":
+        ensure  => installed,
+    }
+
+    package { "qtcurve-kde4":
+        ensure  => installed,
+    }
+
+    package { "scribus":
+        ensure  => installed,
+    }
+
+    package { "system-config-printer-kde":
+        ensure  => installed,
+    }
+
+    package { "xorg-x11-apps":
+        ensure  => installed,
+    }
+
+    package { "xsettings-kde":
+        ensure  => installed,
     }
 
 }
