f232ce3c55b833de7bc13e925a78c4ad498c4da2
John Florian <john_florian@dart.biz>
2010-12-09 10:43:09 -0500

    Fix - build servers package using "localhost"
    The meta-data should report the canonical host name.  This provides
    a sane /etc/hosts file that should help alleviate this and similar
    problems.

--- modules/dart/manifests/classes/build_server_node.pp
+++ modules/dart/manifests/classes/build_server_node.pp
@@ -8,4 +8,16 @@ class dart::build_server_node inherits dart::server_node {
     # TODO: create /j/{git,rpmbuild}
     # Right now it easiest to just rsync /j from another build server.
 
+    # Ensure that package meta-data reports canonical host name rather than
+    # "localhost".
+    file { "/etc/hosts":
+	content	=> template("dart/hosts"),
+        group	=> "root",
+        mode    => "0644",
+        owner   => "root",
+        seluser => "system_u",
+        selrole => "object_r",
+        seltype => "net_conf_t",
+    }
+
 }
