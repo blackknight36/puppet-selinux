80f63a471cc6c292ff78a31ebc359c11947badc7
John Florian <john_florian@dart.biz>
2010-12-28 13:32:02 -0500

    Change - transition mdct-puppet to bacula-5

--- modules/dart/manifests/classes/mdct-puppet.pp
+++ modules/dart/manifests/classes/mdct-puppet.pp
@@ -2,7 +2,7 @@
 
 class dart::mdct-puppet inherits dart::puppet_server_node {
     # passwords generated with bacula-password-generator package
-    $bacula_client_director_password = "0cCamrzZiiA5mOHh3YkffFoymLroVOTU1wr2nmLEPKae"
-    $bacula_client_director_monitor_password = "kPn3DRuaxk9Iwchm0mq4WthqYdwXbI4QI9WLArP3S4nz"
+    $bacula_client_director_password = "d730e38202fb2f6b8f8fb35045538a9f"
+    $bacula_client_director_monitor_password = "db6696b5078c30697ae55a2788f0529d"
     include bacula::client
 }
