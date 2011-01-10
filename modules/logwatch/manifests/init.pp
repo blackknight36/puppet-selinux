68adff5d0e6f7e3968c85d8445db16163e25c400
John Florian <john_florian@dart.biz>
2011-01-10 11:59:44 -0500

    New - manage logwatch's ignore.conf where desired

--- modules/logwatch/manifests/init.pp
+++ modules/logwatch/manifests/init.pp
@@ -6,4 +6,17 @@ class logwatch {
 	ensure	=> installed,
     }
 
+    file { "/etc/logwatch/conf/ignore.conf":
+        group	=> "root",
+        mode    => "0640",
+        owner   => "root",
+        require => Package["logwatch"],
+        source  => [
+            "puppet:///private-host/logwatch/ignore.conf",
+            "puppet:///logwatch/ignore.conf",
+        ],
+    }
+
+    # There is no logwatch "service" to manage; it's just a cron job.
+
 }
