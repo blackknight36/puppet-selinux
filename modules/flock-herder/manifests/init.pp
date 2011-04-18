521c81adbbecd66b5bd1e7989e81e13250ff5f3f
John Florian <john_florian@dart.biz>
2011-04-18 08:18:44 -0400

    Fix - suppress erroneous warning
    See http://projects.puppetlabs.com/issues/2577

--- modules/flock-herder/manifests/init.pp
+++ modules/flock-herder/manifests/init.pp
@@ -6,18 +6,23 @@ class flock-herder {
 	ensure	=> installed,
     }
 
-    file { "/etc/herd.conf":
-        group	=> "root",
-        mode    => "0644",
-        owner   => "root",
-        require => Package["flock-herder"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
-        source  => [
-            "puppet:///private-host/flock-herder/herd.conf",
-            "puppet:///modules/flock-herder/herd.conf",
-        ],
+    # Older puppets cannot cope with this style of source w/o generating
+    # warnings (that really should be info instead).  See
+    # http://projects.puppetlabs.com/issues/2577 for details.
+    if $operatingsystemrelease > 10 {
+        file { "/etc/herd.conf":
+            group	=> "root",
+            mode    => "0644",
+            owner   => "root",
+            require => Package["flock-herder"],
+            seluser => "system_u",
+            selrole => "object_r",
+            seltype => "etc_t",
+            source  => [
+                "puppet:///private-host/flock-herder/herd.conf",
+                "puppet:///modules/flock-herder/herd.conf",
+            ],
+        }
     }
 
 }
