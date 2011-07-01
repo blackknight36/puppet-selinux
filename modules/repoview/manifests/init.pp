ef69e1d0537b663fae7ae46dcf8855aeb3313854
John Florian <john_florian@dart.biz>
2011-07-01 11:57:22 -0400

    New - repoview now generating index for releases
    This new index page makes it easy to browse the MDCT repositories for
    all Fedora releases.

--- modules/repoview/manifests/init.pp
+++ modules/repoview/manifests/init.pp
@@ -2,19 +2,35 @@
 
 class repoview {
 
-    package { "repoview":
+    package { 'repoview':
 	ensure	=> installed,
     }
 
-    file { "/etc/cron.d/mdct-repoview":
-        group	=> "root",
-        mode    => "0644",
-        owner   => "root",
+    file { '/usr/libexec/mdct-repoview':
+        group	=> 'root',
+        mode    => '0755',
+        owner   => 'root',
+        source  => 'puppet:///modules/repoview/mdct-repoview',
+    }
+
+    file { '/var/lib/mdct-repoview':
+        ensure  => 'directory',
+        group	=> 'root',
+        mode    => '0755',
+        owner   => 'root',
+    }
+
+    file { '/etc/cron.d/mdct-repoview':
+        group	=> 'root',
+        mode    => '0644',
+        owner   => 'root',
         require => [
-            #Service["crond"],  # if there were such a module
-            Package["repoview"],
+            #Service['crond'],  # if there were such a module
+            File['/usr/libexec/mdct-repoview'],
+            File['/var/lib/mdct-repoview'],
+            Package['repoview'],
         ],
-        source  => "puppet:///modules/repoview/mdct-repoview",
+        source  => 'puppet:///modules/repoview/mdct-repoview.cron',
     }
 
 }
