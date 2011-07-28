0ab51090c2257d2c641764a59369014fdbaf970f
John Florian <john_florian@dart.biz>
2011-07-28 16:58:15 -0400

    Change - workstations for AH and CP should be KDE

--- modules/dart/manifests/classes/mdct-dev11.pp
+++ modules/dart/manifests/classes/mdct-dev11.pp
@@ -1,5 +1,6 @@
 # /etc/puppet/modules/dart/manifests/classes/mdct-dev11.pp
 
 class dart::mdct-dev11 inherits dart::workstation_node {
+    include packages::kde
     include yum-cron
 }
--- modules/dart/manifests/classes/mdct-dev14.pp
+++ modules/dart/manifests/classes/mdct-dev14.pp
@@ -1,4 +1,6 @@
 # /etc/puppet/modules/dart/manifests/classes/mdct-dev14.pp
 
 class dart::mdct-dev14 inherits dart::workstation_node {
+    include packages::kde
+    include yum-cron
 }
