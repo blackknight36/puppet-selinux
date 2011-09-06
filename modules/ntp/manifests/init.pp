e8f0b7d0505ad1d9681d9ec85e8550ee92b90b69
John Florian <john_florian@dart.biz>
2011-09-06 13:10:02 -0400

    Change - use chrony on VMs

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -3,7 +3,8 @@
 class ntp {
 
     # TODO: drop special condition for mdct-dev12 once on F16 or later
-    if $operatingsystemrelease > 15 or $hostname == 'mdct-dev12' {
+    if $operatingsystemrelease > 15 or $hostname == 'mdct-dev12' or
+       $virtual == 'kvm' {
         $ntp_package = 'chrony'
         $ntp_daemon = 'chronyd'
     } else {
@@ -12,11 +13,11 @@ class ntp {
     }
 
     package { "$ntp_package":
-	ensure	=> installed
+        ensure  => installed
     }
 
     file { "ntp_conf":
-        group	=> 'root',
+        group   => 'root',
         mode    => '0644',
         owner   => 'root',
         path    => "/etc/${ntp_package}.conf",
@@ -25,39 +26,58 @@ class ntp {
     }
 
     if $ntp_package == 'ntp' {
+
         if $operatingsystemrelease < 14 {
             $ntpd_sysconfig = "ntpd.pre-F14"
         } else {
             $ntpd_sysconfig = "ntpd"
         }
+
         file { "/etc/sysconfig/ntpd":
-            before      => Service["$ntp_daemon"],
-            group	=> "root",
+            group       => "root",
             mode        => '0644',
-            notify      => Service["$ntp_daemon"],
             owner       => 'root',
             require     => Package["$ntp_package"],
             source      => "puppet:///ntp/${ntpd_sysconfig}",
         }
-    }
 
-    service { "$ntp_daemon":
-        enable          => $virtual ? {
-            "kvm"       => false,
-            default     => true,
-        },
-        ensure          => $virtual ? {
-            "kvm"       => stopped,
-            default     => running,
-        },
-	hasrestart	=> true,
-	hasstatus	=> true,
-	require		=> [
-	    Package["$ntp_package"],
-	],
-	subscribe	=> [
-	    File['ntp_conf'],
-	],
+        # ntp does not deal well with jitter of VM clocks
+        service { "$ntp_daemon":
+            enable      => $virtual ? {
+                "kvm"   => false,
+                default => true,
+            },
+            ensure      => $virtual ? {
+                "kvm"   => stopped,
+                default => running,
+            },
+            hasrestart  => true,
+            hasstatus   => true,
+            require             => [
+                Package["$ntp_package"],
+            ],
+            subscribe   => [
+                File['ntp_conf'],
+                File['/etc/sysconfig/ntpd'],
+            ],
+        }
+
+    } else {
+
+        # chrony deals well with jitter of VM clocks
+        service { "$ntp_daemon":
+            enable      => true,
+            ensure      => running,
+            hasrestart  => true,
+            hasstatus   => true,
+            require             => [
+                Package["$ntp_package"],
+            ],
+            subscribe   => [
+                File['ntp_conf'],
+            ],
+        }
+
     }
 
 }
