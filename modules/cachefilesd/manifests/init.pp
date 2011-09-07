98c7a6fa0fc3e7f5ac38bbe479160ca9051a682b
John Florian <john_florian@dart.biz>
2011-09-07 16:52:50 -0400

    Drop - cachefilesd again for mdct-dev12
    Once again this is causing kernel oopses for me.  Not sure I ever want
    it back, it does more harm than good.

--- modules/cachefilesd/manifests/init.pp
+++ modules/cachefilesd/manifests/init.pp
@@ -23,12 +23,12 @@ class cachefilesd {
         service { "cachefilesd":
             enable      => $hostname ? {
                 #"mdct-dev10"    => false,
-                #"mdct-dev12"    => false,
+                "mdct-dev12"    => false,
                 default         => true,
             },
             ensure      => $hostname ? {
                 #"mdct-dev10"    => stopped,
-                #"mdct-dev12"    => stopped,
+                "mdct-dev12"    => stopped,
                 default         => running,
             },
             hasrestart  => true,
