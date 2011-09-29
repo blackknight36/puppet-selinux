31e8cd178d3f134be0785297aaac6708cf788356
John Florian <john_florian@dart.biz>
2011-09-29 16:40:23 -0400

    New - custom fact 'plant_number'
    This fact was derived from one that is nearly identical in the AOS
    resources.  Here it returns nil though if the plant number cannot be
    determined as that's more appropriate.

---
+++ modules/dart/lib/facter/plant_number.rb
@@ -0,0 +1,19 @@
+# Synopsis:
+#       Determine the plant number by parsing out the 2nd octet of the host's
+#       default route.  The value returned will be a two digit code,
+#       left-padded with zeroes, e.g., (Mason ==> "01").  If no default route
+#       exists, a value of "01" (Mason) will be returned.
+
+Facter.add("plant_number") do
+    begin
+        gw = IO.popen("/sbin/ip route show 0.0.0.0/0").read().split()[2]
+        if gw
+            plant = "%02d" % gw.split(".")[1]
+        else
+            plant = nil
+        end
+    end
+    setcode do
+        plant
+    end
+end
