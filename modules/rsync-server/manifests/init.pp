558b17b8056ab23e3b8b2606654645eb3df88c31
John Florian <john_florian@dart.biz>
2011-10-05 14:46:51 -0400

    Change - rsync-server module cont'd development
    This should complete basic development of this module.
    
    - correct xinted configuration so that rsync is enabled
    - add private-host path for xinetd configuration
    - add support for the rsyncd.conf so as to expose exports
    - configure SELinux appropriately

--- modules/rsync-server/manifests/init.pp
+++ modules/rsync-server/manifests/init.pp
@@ -5,21 +5,38 @@ class rsync-server {
     include lokkit
     include xinetd
 
-    package { "rsync":
+    package { 'rsync':
 	ensure	=> installed,
     }
 
-    file { "/etc/xinetd.d/rsync":
-        group	=> "root",
-        mode    => 644,
-        notify  => Service["xinetd"],
-        owner   => "root",
-        require => Package["rsync"],
-        source  => "puppet:///rsync-server/rsync",
+    file { '/etc/xinetd.d/rsync':
+        group	=> 'root',
+        mode    => '0644',
+        notify  => Service['xinetd'],
+        owner   => 'root',
+        require => Package['rsync'],
+        source  => [
+            'puppet:///private-host/xinetd.d/rsync',
+            'puppet:///rsync-server/rsync',
+        ],
     }
 
-    lokkit::tcp_port { "rsync":
-        port    => "873",
+    file { '/etc/rsyncd.conf':
+        group	=> 'root',
+        mode    => '0644',
+        notify  => Service['xinetd'],
+        owner   => 'root',
+        require => Package['rsync'],
+        source  => 'puppet:///private-host/rsync-server/rsyncd.conf',
+    }
+
+    lokkit::tcp_port { 'rsync':
+        port    => '873',
+    }
+
+    selboolean { 'rsync_export_all_ro':
+        persistent      => true,
+        value           => on,
     }
 
 }
