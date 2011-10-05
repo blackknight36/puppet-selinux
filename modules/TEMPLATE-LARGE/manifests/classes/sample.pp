04dc0a9d452c9956b95bc2fe2aa0ef407ba978b1
John Florian <john_florian@dart.biz>
2011-10-05 13:32:29 -0400

    Janitorial - module template improvements
    - prefer single-quotes where variable expansion not required
    - use relative path in header; easier to update and remains accurate
      should the manifests ever be relocated.
    - add private-domain to source paths for file resources

--- modules/TEMPLATE-LARGE/manifests/classes/sample.pp
+++ modules/TEMPLATE-LARGE/manifests/classes/sample.pp
@@ -1,4 +1,4 @@
-# /etc/puppet/modules/MODULE_NAME/manifests/classes/CLASS_NAME.pp
+# modules/MODULE_NAME/manifests/classes/CLASS_NAME.pp
 #
 # Synopsis:
 #       Configures a host as a CLASS_NAME server.
@@ -7,7 +7,7 @@
 #       NONE
 #
 # Requires:
-#       Class["postgresql::server"]     <= Use this notation for other resources
+#       Class['postgresql::server']     <= Use this notation for other resources
 #
 #       $MODULE_NAME_var1                       Abstract variable 1
 #       $MODULE_NAME_var2                       Abstract variable 2
@@ -15,9 +15,9 @@
 #
 # Example usage:
 #
-#       $MODULE_NAME_var1 = "X_foo"
-#       $MODULE_NAME_var2 = "X_bar"
-#       $MODULE_NAME_CONFIG_NAME_source = "puppet:///private-host/CONFIG_NAME"
+#       $MODULE_NAME_var1 = 'X_foo'
+#       $MODULE_NAME_var2 = 'X_bar'
+#       $MODULE_NAME_CONFIG_NAME_source = 'puppet:///private-host/CONFIG_NAME'
 #       include MODULE_NAME::CLASS_NAME
 
 class MODULE_NAME::CLASS_NAME {
@@ -25,67 +25,68 @@ class MODULE_NAME::CLASS_NAME {
     include lokkit
 
     case $MODULE_NAME_CONFIG_NAME_source {
-        "": {
-            fail("Required \$MODULE_NAME_CONFIG_NAME_source variable is not defined")
+        '': {
+            fail('Required $MODULE_NAME_CONFIG_NAME_source variable is not defined')
         }
     }
 
-    package { "PACKAGE_NAME":
+    package { 'PACKAGE_NAME':
 	ensure	=> installed,
     }
 
-    package { "CONFLICTING_PACKAGE_NAME":
+    package { 'CONFLICTING_PACKAGE_NAME':
 	ensure	=> absent,
 	# It may be necessary to have the replacement installed prior to
 	# removal of the conflicting package.
-        require => Package["PACKAGE_NAME"],
+        require => Package['PACKAGE_NAME'],
     }
 
     # static file
-    file { "/CONFIG_PATH/CONFIG_NAME":
+    file { '/CONFIG_PATH/CONFIG_NAME':
         # don't forget to verify these!
-        group	=> "root",
-        mode    => "0640",
-        owner   => "root",
-        require => Package["PACKAGE_NAME"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
+        group	=> 'root',
+        mode    => '0640',
+        owner   => 'root',
+        require => Package['PACKAGE_NAME'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'etc_t',
         source  => [
-            "puppet:///private-host/MODULE_NAME/CONFIG_NAME",
-            "puppet:///modules/MODULE_NAME/CONFIG_NAME",
+            'puppet:///private-host/MODULE_NAME/CONFIG_NAME',
+            'puppet:///private-domain/MODULE_NAME/CONFIG_NAME',
+            'puppet:///modules/MODULE_NAME/CONFIG_NAME',
         ],
     }
 
     # template file
-    file { "/CONFIG_PATH/CONFIG_NAME":
-	content	=> template("MODULE_NAME/CONFIG_NAME"),
+    file { '/CONFIG_PATH/CONFIG_NAME':
+	content	=> template('MODULE_NAME/CONFIG_NAME'),
         # don't forget to verify these!
-        group	=> "root",
-        mode    => "0640",
-        owner   => "root",
-        require => Package["PACKAGE_NAME"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
+        group	=> 'root',
+        mode    => '0640',
+        owner   => 'root',
+        require => Package['PACKAGE_NAME'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'etc_t',
     }
 
-    lokkit::tcp_port { "SERVICE_NAME":
-        port    => "SERVICE_PORT",
+    lokkit::tcp_port { 'SERVICE_NAME':
+        port    => 'SERVICE_PORT',
     }
 
-    service { "SERVICE_NAME":
+    service { 'SERVICE_NAME':
         enable		=> true,
         ensure		=> running,
         hasrestart	=> true,
         hasstatus	=> true,
         require		=> [
-            Exec["open-SERVICE_NAME-tcp-port"],
-            Package["CONFLICTING_PACKAGE_NAME"],
-            Package["PACKAGE_NAME"],
+            Exec['open-SERVICE_NAME-tcp-port'],
+            Package['CONFLICTING_PACKAGE_NAME'],
+            Package['PACKAGE_NAME'],
         ],
         subscribe	=> [
-            File["/CONFIG_PATH/CONFIG_NAME"],
+            File['/CONFIG_PATH/CONFIG_NAME'],
         ],
     }
 
--- modules/TEMPLATE-LARGE/manifests/definitions/site-config.pp
+++ modules/TEMPLATE-LARGE/manifests/definitions/site-config.pp
@@ -1,4 +1,4 @@
-# /etc/puppet/modules/MODULE_NAME/manifests/definitions/DEFINE_NAME.pp
+# modules/MODULE_NAME/manifests/definitions/DEFINE_NAME.pp
 #
 # Synopsis:
 #       Installs a web-site configuration file for the Apache web server.
@@ -8,29 +8,29 @@
 #       source:         The puppet URI for obtaining the web site's configuration file.
 #
 # Requires:
-#       Class["MODULE_NAME"]
+#       Class['MODULE_NAME']
 #
 # Example usage:
 #
 #       include MODULE_NAME
 #
-#       MODULE_NAME::DEFINE_NAME { "acme":
-#           notify  => Service["SERVICE_NAME"],
-#           source  => "puppet:///private-host/acme.conf",
+#       MODULE_NAME::DEFINE_NAME { 'acme':
+#           notify  => Service['SERVICE_NAME'],
+#           source  => 'puppet:///private-host/acme.conf',
 #       }
 
 
-define MODULE_NAME::DEFINE_NAME ($ensure="present", $source) {
+define MODULE_NAME::DEFINE_NAME ($ensure='present', $source) {
 
     file { "/CONFIG_PATH/conf.d/${name}.conf":
         ensure  => $ensure,
-        owner   => "root",
-        group   => "root",
-        mode    => "0640",
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "httpd_config_t",
-        require => Package["PACKAGE_NAME"],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'httpd_config_t',
+        require => Package['PACKAGE_NAME'],
         source  => "${source}",
     }
 
