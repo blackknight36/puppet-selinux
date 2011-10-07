c3636bc09da31ba819914815c4ab3e667641ae1d
John Florian <john_florian@dart.biz>
2011-10-07 13:44:28 -0400

    New - mdct-02ut as a plant_utility_server_node
    This manages mdct-02ut as a plant_utility_server_node which means that
    it is tasked with maintaining a local cache of MDC's weather imagery,
    merging that with content that is targeted for specific media-playback
    devices as well as content targeted for all such devices and then
    disbursing said content into directories unique to each display device
    for their later retrieval.

---
+++ modules/dart/manifests/classes/mdct-02ut.pp
@@ -0,0 +1,5 @@
+# /etc/puppet/modules/dart/manifests/classes/mdct-02ut.pp
+
+class dart::mdct-02ut inherits dart::plant_utility_server_node {
+
+}
---
+++ modules/dart/manifests/classes/plant_utility_server_node.pp
@@ -0,0 +1,21 @@
+# /etc/puppet/modules/dart/manifests/classes/plant_utility_server_node.pp
+#
+# Synopsis:
+#       This class is used primarily for servers that provided shared access
+#       to media content used by the AOS media-playback devices.
+
+class dart::plant_utility_server_node inherits dart::server_node {
+
+    # AOS devices pull media-playback content using rsync.
+    include rsync-server
+
+    # That same media-playback content is pushed in via Windows systems.
+    include samba
+
+    # Additional content comes in the form of MDC's weather imagery.  The
+    # media-disbursal module knows how to retrieve that imagery and merge it
+    # with the content from the Windows system and disburse all such content
+    # in a location where media-playback clients can utilize it.
+    include media-disbursal
+
+}
