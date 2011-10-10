7a9036e41862ee5f42a5798af552371ea48330ea
John Florian <john_florian@dart.biz>
2011-10-10 14:04:56 -0400

    New - support for puppet-report-cleanup
    puppet-tools-0.5-1 brings the ability to limit pool size of puppet's
    reports.

--- modules/puppet/manifests/classes/server.pp
+++ modules/puppet/manifests/classes/server.pp
@@ -4,57 +4,72 @@ class puppet::server {
 
     include lokkit
 
-    package { "puppet-server":
+    package { 'puppet-server':
 	ensure	=> installed,
     }
 
-    package { "puppet-tools":
+    package { 'puppet-tools':
 	ensure	=> installed,
     }
 
-    file { "/etc/puppet/fileserver.conf":
-        group	=> "root",
-        mode    => "0644",
-        owner   => "root",
-        require => Package["puppet-server"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "puppet_etc_t",
-        # This one can be a bit of a "chicken vs. egg" problem.  There's
+    file { '/etc/puppet/fileserver.conf':
+        group	=> 'root',
+        mode    => '0644',
+        owner   => 'root',
+        require => Package['puppet-server'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'puppet_etc_t',
+        # This one can be a bit of a 'chicken vs. egg' problem.  There's
         # little point in providing a source here since the target is already
         # within git.
-        #       source  => "puppet:///modules/puppet/fileserver.conf",
+        #       source  => 'puppet:///modules/puppet/fileserver.conf',
     }
 
-    file { "/etc/sysconfig/puppetmaster":
-        group	=> "root",
-        mode    => "0640",
-        owner   => "root",
-        require => Package["puppet-server"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
-        source  => "puppet:///modules/puppet/puppetmaster",
+    file { '/etc/sysconfig/puppetmaster':
+        group	=> 'root',
+        mode    => '0640',
+        owner   => 'root',
+        require => Package['puppet-server'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'etc_t',
+        source  => 'puppet:///modules/puppet/puppetmaster',
     }
 
-    # All other puppet resources are managed via GIT "in place".
+    # All other puppet resources are managed via GIT 'in place'.
 
-    lokkit::tcp_port { "puppetmaster":
-        port    => "8140",
+    lokkit::tcp_port { 'puppetmaster':
+        port    => '8140',
     }
 
-    service { "puppetmaster":
+    service { 'puppetmaster':
         enable		=> true,
         ensure		=> running,
         hasrestart	=> true,
         hasstatus	=> true,
         require		=> [
-            Exec["open-puppetmaster-tcp-port"],
-            Package["puppet-server"],
+            Exec['open-puppetmaster-tcp-port'],
+            Package['puppet-server'],
         ],
         subscribe	=> [
-            File["/etc/puppet/fileserver.conf"],
-            File["/etc/sysconfig/puppetmaster"],
+            File['/etc/puppet/fileserver.conf'],
+            File['/etc/sysconfig/puppetmaster'],
+        ],
+    }
+
+    file { '/etc/cron.d/puppet-report-cleanup':
+        group	=> 'root',
+        mode    => '0644',
+        owner   => 'root',
+        require => Package['puppet-tools'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'system_cron_spool_t',
+        source  => [
+            'puppet:///private-host/puppet/puppet-report-cleanup.cron',
+            'puppet:///private-domain/puppet/puppet-report-cleanup.cron',
+            'puppet:///modules/puppet/puppet-report-cleanup.cron',
         ],
     }
 
