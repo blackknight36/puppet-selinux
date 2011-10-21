4a7ee2fe710cdbc55744782aa917b7839ac0d4c2
John Florian <john_florian@dart.biz>
2011-10-21 10:46:08 -0400

    Revert "Drop - mdct-00dw from node manifest"
    This reverts commit adb30b0c2fa47ccfdd09a68fce5e93ab3956b161.
    
    Just noticed the puppetmaster log showed that it was attempting to apply
    the catalog, so it must be that puppet was temporarily disabled.
    Reinstating.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,6 +6,7 @@
 
 node "jflo-f13.dartcontainer.com"               { include dart::jflo-f13 }
 node "mdct-00bk2.dartcontainer.com"             { include dart::mdct-00bk2 }
+node "mdct-00dw.dartcontainer.com"              { include dart::mdct-00dw }
 node "mdct-00fs.dartcontainer.com"              { include dart::mdct-00fs }
 node "mdct-02ut.dartcontainer.com"              { include dart::mdct-02ut }
 node "mdct-04pt.dartcontainer.com"              { include dart::mdct-04pt }
