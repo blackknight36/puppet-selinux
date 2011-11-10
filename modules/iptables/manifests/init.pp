e43787a088b91a69cdc42ef1df3a3f91f122faa4
John Florian <john_florian@dart.biz>
2011-11-10 13:38:21 -0500

    Fix - iptables module does not manage ip6tables

--- modules/iptables/manifests/init.pp
+++ modules/iptables/manifests/init.pp
@@ -1,10 +1,10 @@
 # modules/iptables/manifests/init.pp
 #
 # Synopsis:
-#       Configures the iptables service on a host.
+#       Configures the iptables and ip6tables services on a host.
 #
 # Parameters:
-#       $enabled       service enabled: true (default) or false
+#       $enabled       services enabled: true (default) or false
 #
 # Requires:
 #
@@ -16,7 +16,7 @@
 
 class iptables($enabled, $kernel_modules='') {
 
-    package { 'iptables':
+    package { ['iptables', 'iptables-ipv6']:
         ensure  => installed,
     }
 
@@ -26,6 +26,17 @@ class iptables($enabled, $kernel_modules='') {
         mode    => '0600',
         owner   => 'root',
         require => Package['iptables'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'system_conf_t',
+    }
+
+    file { '/etc/sysconfig/ip6tables-config':
+	content	=> template('iptables/ip6tables-config'),
+        group	=> 'root',
+        mode    => '0600',
+        owner   => 'root',
+        require => Package['iptables-ipv6'],
         seluser => 'system_u',
         selrole => 'object_r',
         seltype => 'system_conf_t',
@@ -43,6 +54,20 @@ class iptables($enabled, $kernel_modules='') {
         # number
         status          => '(iptables -L --line-numbers; iptables -L -t nat --line-numbers) | grep -q "^[0-9]"',
         subscribe       => File['/etc/sysconfig/iptables-config'],
+    }
+
+    service { 'ip6tables':
+        enable          => $enabled,
+        ensure          => $enabled,
+        hasrestart      => true,
+        hasstatus       => false,   # it does, but always exits 0
+        require         => [
+            Package['iptables-ipv6'],
+        ],
+        # weak strategy, best so far: look for rules prefixed with a line
+        # number
+        status          => 'ip6tables -L --line-numbers | grep -q "^[0-9]"',
+        subscribe       => File['/etc/sysconfig/ip6tables-config'],
     }
 
 }
