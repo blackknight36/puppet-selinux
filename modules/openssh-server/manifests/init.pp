8a644792eb161b2293200bcc6a891c8dc580d2ce
John Florian <john_florian@dart.biz>
2011-11-11 14:07:13 -0500

    Refactor - relocate sshd_config for mdct-dev6 to private-host
    Moving away from having host-specific details within the otherwise
    generic modules.

--- modules/openssh-server/manifests/init.pp
+++ modules/openssh-server/manifests/init.pp
@@ -1,40 +1,54 @@
-# /etc/puppet/modules/openssh-server/manifests/init.pp
+# modules/openssh-server/manifests/init.pp
+#
+# Synopsis:
+#       Configures a host as a OpenSSH server server.
+#
+# Parameters:
+#       NONE
+#
+# Requires:
+#       NONE
+#
+# Example usage:
+#
+#       include 'openssh-server'
 
 class openssh-server {
 
     include lokkit
 
-    package { "openssh-server":
+    package { 'openssh-server':
 	ensure	=> installed,
     }
 
-    file { "/etc/ssh/sshd_config":
-        group	=> "root",
+    file { '/etc/ssh/sshd_config':
+        group	=> 'root',
         mode    => 600,
-        owner   => "root",
-        require => Package["openssh-server"],
+        owner   => 'root',
+        require => Package['openssh-server'],
 	source	=> [
-            "puppet:///openssh-server/sshd_config.$hostname",
+            'puppet:///private-host/openssh-server/sshd_config',
+            'puppet:///private-domain/openssh-server/sshd_config',
             "puppet:///openssh-server/sshd_config.$operatingsystem.$operatingsystemrelease",
-            "puppet:///openssh-server/sshd_config",
+            'puppet:///openssh-server/sshd_config',
         ],
     }
 
-    lokkit::tcp_port { "ssh":
-        port    => "22",
+    lokkit::tcp_port { 'ssh':
+        port    => '22',
     }
 
-    service { "sshd":
+    service { 'sshd':
         enable		=> true,
         ensure		=> running,
         hasrestart	=> true,
         hasstatus	=> true,
         require		=> [
-            Exec["open-ssh-tcp-port"],
-            Package["openssh-server"],
+            Exec['open-ssh-tcp-port'],
+            Package['openssh-server'],
         ],
         subscribe	=> [
-            File["/etc/ssh/sshd_config"],
+            File['/etc/ssh/sshd_config'],
         ]
     }
 
