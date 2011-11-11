94b02d8ecb830fe5320065f1fdd6c10a58ae54b4
John Florian <john_florian@dart.biz>
2011-11-11 09:32:38 -0500

    Janitorial - preferred quoting style

--- modules/yum/manifests/init.pp
+++ modules/yum/manifests/init.pp
@@ -1,4 +1,4 @@
-# /etc/puppet/modules/yum/manifests/init.pp
+# modules/yum/manifests/init.pp
 
 class yum {
 
@@ -9,41 +9,33 @@ class yum {
 	}
     }
 
-# TODO: need local mirror
-#     config_repo {"adobe":
-# 	server_uri	=> "http://linuxdownload.adobe.com/adobe-release",
-#         # Yes, it's i386 and noarch, really!  Sigh ...
-# 	pkg_name	=> "adobe-release-i386",
-# 	pkg_release	=> "1.0-1.noarch",
-#     }
+    $pub = 'http://mdct-00fs.dartcontainer.com/ftp/pub/fedora'
 
-    $pub = "http://mdct-00fs.dartcontainer.com/ftp/pub/fedora"
-
-    config_repo {"local-fedora":
+    config_repo {'local-fedora':
 	server_uri	=> "$pub/mdct/${operatingsystemrelease}/${architecture}",
-	pkg_name	=> "yum-local-mirror-conf",
+	pkg_name	=> 'yum-local-mirror-conf',
 	pkg_release	=> $operatingsystemrelease ? {
-	    "8" 	=> "8-1.dcc.noarch",
-	    "10"	=> "10-2.dcc.noarch",
-	    "11"	=> "11-1.dcc.noarch",
-	    "12"	=> "12-1.dcc.noarch",
-	    "13"	=> "13-1.dcc.noarch",
-	    "14"	=> "14-1.dcc.noarch",
-	    "15"	=> "15-1.dcc.noarch",
+	    '8' 	=> '8-1.dcc.noarch',
+	    '10'	=> '10-2.dcc.noarch',
+	    '11'	=> '11-1.dcc.noarch',
+	    '12'	=> '12-1.dcc.noarch',
+	    '13'	=> '13-1.dcc.noarch',
+	    '14'	=> '14-1.dcc.noarch',
+	    '15'	=> '15-1.dcc.noarch',
             },
     }
 
-    config_repo {"mdct":
+    config_repo {'mdct':
         server_uri	=> "$pub/mdct/${operatingsystemrelease}/${architecture}",
-        pkg_name	=> "fedora-mdct-release",
+        pkg_name	=> 'fedora-mdct-release',
         pkg_release	=> $operatingsystemrelease ? {
-            "8"		=> "8-1.dcc.noarch",
-            "10"	=> "10-3.dcc.noarch",
-            "11"	=> "11-1.dcc.noarch",
-            "12"	=> "12-1.dcc.noarch",
-            "13"	=> "13-1.dcc.noarch",
-            "14"	=> "14-1.dcc.noarch",
-            "15"	=> "15-1.dcc.noarch",
+            '8'		=> '8-1.dcc.noarch',
+            '10'	=> '10-3.dcc.noarch',
+            '11'	=> '11-1.dcc.noarch',
+            '12'	=> '12-1.dcc.noarch',
+            '13'	=> '13-1.dcc.noarch',
+            '14'	=> '14-1.dcc.noarch',
+            '15'	=> '15-1.dcc.noarch',
             },
     }
 
@@ -51,31 +43,31 @@ class yum {
     # Fedora 8.
     if $operatingsystemrelease >= 10 {
 
-	config_repo {"rpmfusion-free":
+	config_repo {'rpmfusion-free':
 	    server_uri	=> "$pub/rpmfusion/free/fedora/releases/${operatingsystemrelease}/Everything/${architecture}/os/",
-	    pkg_name	=> "rpmfusion-free-release",
+	    pkg_name	=> 'rpmfusion-free-release',
 	    pkg_release	=> $operatingsystemrelease ? {
-		"10"	=> "10-1.noarch",
-		"11"	=> "11-1.noarch",
-		"12"	=> "12-1.noarch",
-		"13"	=> "13-2.noarch",
-		"14"	=> "14-0.4.noarch",
-		"15"	=> "15-1.noarch",
+		'10'	=> '10-1.noarch',
+		'11'	=> '11-1.noarch',
+		'12'	=> '12-1.noarch',
+		'13'	=> '13-2.noarch',
+		'14'	=> '14-0.4.noarch',
+		'15'	=> '15-1.noarch',
 		},
 	}
 
-	config_repo {"rpmfusion-nonfree":
+	config_repo {'rpmfusion-nonfree':
 	    server_uri	=> "$pub/rpmfusion/nonfree/fedora/releases/${operatingsystemrelease}/Everything/${architecture}/os/",
-	    pkg_name	=> "rpmfusion-nonfree-release",
+	    pkg_name	=> 'rpmfusion-nonfree-release',
 	    pkg_release	=> $operatingsystemrelease ? {
-		"10" 	=> "10-1.noarch",
-		"11" 	=> "11-1.noarch",
-		"12" 	=> "12-1.noarch",
-		"13" 	=> "13-2.noarch",
-		"14" 	=> "14-0.4.noarch",
-		"15" 	=> "15-1.noarch",
+		'10' 	=> '10-1.noarch',
+		'11' 	=> '11-1.noarch',
+		'12' 	=> '12-1.noarch',
+		'13' 	=> '13-2.noarch',
+		'14' 	=> '14-0.4.noarch',
+		'15' 	=> '15-1.noarch',
 		},
-	    require	=> Exec["config-repo-rpmfusion-free"],
+	    require	=> Exec['config-repo-rpmfusion-free'],
 	}
 
     }
