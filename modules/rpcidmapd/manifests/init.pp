52ac6bf7317e9b0ee942202a1f61b2010f0dd2aa
John Florian <john_florian@dart.biz>
2011-11-11 11:34:17 -0500

    Fix - service no longer named rpcidmapd in Fedora 16
    This has been renamed to nfs-idmap by systemd.  All the underlying bits
    seem to be basically the same.

--- modules/rpcidmapd/manifests/init.pp
+++ modules/rpcidmapd/manifests/init.pp
@@ -1,40 +1,46 @@
-# /etc/puppet/modules/rpcidmapd/manifests/init.pp
+# modules/rpcidmapd/manifests/init.pp
 
 class rpcidmapd {
 
-    package { "nfs-utils":
+    package { 'nfs-utils':
 	 ensure => installed,
     }
 
-    if $operatingsystem == "Fedora" and $operatingsystemrelease >= 15 {
-        $libnfsidmap_package = "libnfsidmap"
+    if $operatingsystem == 'Fedora' and $operatingsystemrelease >= 15 {
+        $libnfsidmap_package = 'libnfsidmap'
     } else {
-        $libnfsidmap_package = "nfs-utils-lib"
+        $libnfsidmap_package = 'nfs-utils-lib'
     }
-    package { "nfs-utils-lib":
+    package { 'nfs-utils-lib':
 	 ensure => installed,
          name   => $libnfsidmap_package,
     }
 
-    file { "/etc/idmapd.conf":
-        group   => "root",
+    file { '/etc/idmapd.conf':
+        group   => 'root',
         mode    => 644,
-        owner   => "root",
-	require => Package["nfs-utils-lib"],
-        source	=> "puppet:///rpcidmapd/idmapd.conf",
+        owner   => 'root',
+	require => Package['nfs-utils-lib'],
+        source	=> 'puppet:///rpcidmapd/idmapd.conf',
     }
 
-    service { "rpcidmapd":
+    if $operatingsystem == 'Fedora' and $operatingsystemrelease >= 16 {
+        $rpcidmapd_service = 'nfs-idmap'
+    } else {
+        $rpcidmapd_service = 'rpcidmapd'
+    }
+    service { 'rpcidmapd':
 	enable		=> true,
 	ensure		=> running,
 	hasrestart	=> true,
 	hasstatus	=> true,
+        name            => $rpcidmapd_service,
 	require		=> [
-	    Package["nfs-utils"],
-	    Package["nfs-utils-lib"],
+	    Package['nfs-utils'],
+	    Package['nfs-utils-lib'],
 	],
 	subscribe	=> [
-	    File["/etc/idmapd.conf"],
+	    File['/etc/idmapd.conf'],
 	]
     }
 
