ad72d7a815e58a3d981e01f4d8809e2c927d9663
John Florian <john_florian@dart.biz>
2011-11-14 13:44:56 -0500

    Janitorial - use preferred quoting style

--- modules/authconfig/manifests/init.pp
+++ modules/authconfig/manifests/init.pp
@@ -1,55 +1,55 @@
-# /etc/puppet/modules/authconfig/manifests/init.pp
+# modules/authconfig/manifests/init.pp
 
 class authconfig {
 
-    package { "authconfig":
+    package { 'authconfig':
          ensure => installed
     }
 
-    package { "pam":
+    package { 'pam':
         ensure  => installed,
     }
 
-    file { "/etc/pam.d/system-auth-ac":
-        before  => Exec["authconfig"],
-        group   => "root",
+    file { '/etc/pam.d/system-auth-ac':
+        before  => Exec['authconfig'],
+        group   => 'root',
         mode    => 644,
-        owner   => "root",
-        require => Package["pam"],
+        owner   => 'root',
+        require => Package['pam'],
         source  => [
             "puppet:///authconfig/system-auth-ac.$operatingsystem.$operatingsystemrelease",
-            "puppet:///authconfig/system-auth-ac",
+            'puppet:///authconfig/system-auth-ac',
         ],
     }
 
-    if $operatingsystem == "Fedora" {
+    if $operatingsystem == 'Fedora' {
         if $operatingsystemrelease < 13 {
 
-            package { "nss_ldap":
+            package { 'nss_ldap':
                 ensure  => installed
             }
 
-            package { "nscd":
+            package { 'nscd':
                 ensure  => installed,
-                require => Package["nss_ldap"],
+                require => Package['nss_ldap'],
             }
 
-            service { "nscd":
+            service { 'nscd':
                 enable          => true,
                 ensure          => running,
                 hasrestart      => true,
                 hasstatus       => true,
-                require         => Package["nscd"],
+                require         => Package['nscd'],
             }
 
-            exec { "authconfig":
-                command => 'authconfig --enableldap --enableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --updateall',
+            exec { 'authconfig':
+                command => 'authconfig --enableldap --enableldapauth --ldapserver='ldap://10.1.192.106' --ldapbasedn='dc=dartcontainer,dc=com' --disableldaptls --updateall',
                 require => [
-                    File["/etc/pam.d/system-auth-ac"],
-                    Package["authconfig"],
-                    Service["nscd"],
+                    File['/etc/pam.d/system-auth-ac'],
+                    Package['authconfig'],
+                    Service['nscd'],
                 ],
-                unless => "grep -q USELDAPAUTH=yes /etc/sysconfig/authconfig"
+                unless => 'grep -q USELDAPAUTH=yes /etc/sysconfig/authconfig'
             }
 
         } else { # this is Fedora 13 or later
@@ -59,32 +59,32 @@ class authconfig {
             # NFS.  Interestingly, this only affects the cd within login as it
             # is still possible to enter the home dir after login when this is
             # off.
-            if $selinux == "true" {
-                selboolean { "use_nfs_home_dirs":
+            if $selinux == 'true' {
+                selboolean { 'use_nfs_home_dirs':
                     persistent      => true,
                     value           => on,
                 }
             }
 
-            package { [ "krb5-libs", 'pam_krb5' ]:
+            package { [ 'krb5-libs', 'pam_krb5' ]:
                 ensure  => installed,
             }
 
             # As of Fedora 13, nscd (and it's dependent nss_ldap) is being
             # replaced by sssd, but oddly the packaging doesn't obsolete it,
             # so we do so here.
-            package { "nss_ldap":
+            package { 'nss_ldap':
                 ensure  => absent,
             }
 
-            package { "nscd":
+            package { 'nscd':
                 ensure  => absent,
-                require => Package["nss_ldap"],
+                require => Package['nss_ldap'],
             }
 
-            package { "sssd":
+            package { 'sssd':
                 ensure  => installed,
-                require => Package["nscd"],
+                require => Package['nscd'],
             }
 
             # As of Fedora 13, encryption is required to a directory server if
@@ -92,47 +92,47 @@ class authconfig {
             # doesn't provide encryption, authentication is configured for
             # Kerberos against the Active Directory while all other user
             # account information continues to be obtained from LDAP.
-            exec { "authconfig":
-                before  => File["/etc/sssd/sssd.conf"],
-                command => 'authconfig --enableldap --disableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --enablesssd --disablesssdauth --enablekrb5 --krb5realm=DARTCONTAINER.COM --krb5kdc=dartcontainer.com --enablecachecreds --updateall --disablefingerprint',
+            exec { 'authconfig':
+                before  => File['/etc/sssd/sssd.conf'],
+                command => 'authconfig --enableldap --disableldapauth --ldapserver='ldap://10.1.192.106' --ldapbasedn='dc=dartcontainer,dc=com' --disableldaptls --enablesssd --disablesssdauth --enablekrb5 --krb5realm=DARTCONTAINER.COM --krb5kdc=dartcontainer.com --enablecachecreds --updateall --disablefingerprint',
                 require => [
-                    File["/etc/pam.d/system-auth-ac"],
-                    Package["authconfig"],
-                    Package["krb5-libs"],
-                    Package["nscd"],
-                    Package["pam_krb5"],
-                    Package["sssd"],
+                    File['/etc/pam.d/system-auth-ac'],
+                    Package['authconfig'],
+                    Package['krb5-libs'],
+                    Package['nscd'],
+                    Package['pam_krb5'],
+                    Package['sssd'],
                 ],
-                unless => "grep -q USEKERBEROS=yes /etc/sysconfig/authconfig"
+                unless => 'grep -q USEKERBEROS=yes /etc/sysconfig/authconfig'
             }
 
             # According to F13's authconfig(8), authconfig does NOT configure
             # the domain in sssd.conf; it must be done manually.  Hence
             # a starter sssd.conf is provided here.
-            if $operatingsystem == "Fedora" and $operatingsystemrelease >= 15 {
-                $sssd_conf = "sssd.conf.Fedora.15+"
+            if $operatingsystem == 'Fedora' and $operatingsystemrelease >= 15 {
+                $sssd_conf = 'sssd.conf.Fedora.15+'
             } else {
-                $sssd_conf = "sssd.conf"
+                $sssd_conf = 'sssd.conf'
             }
-            file { "/etc/sssd/sssd.conf":
-                group   => "root",
-                mode    => "0600",
-                owner   => "root",
-                require => Package["sssd"],
+            file { '/etc/sssd/sssd.conf':
+                group   => 'root',
+                mode    => '0600',
+                owner   => 'root',
+                require => Package['sssd'],
                 source  => "puppet:///authconfig/$sssd_conf",
             }
 
-            service { "sssd":
+            service { 'sssd':
                 enable          => true,
                 ensure          => running,
                 hasrestart      => true,
                 hasstatus       => true,
                 require         => [
-                    Exec["authconfig"],
-                    Package["sssd"],
+                    Exec['authconfig'],
+                    Package['sssd'],
                 ],
                 subscribe       => [
-                    File["/etc/sssd/sssd.conf"],
+                    File['/etc/sssd/sssd.conf'],
                 ],
             }
 
