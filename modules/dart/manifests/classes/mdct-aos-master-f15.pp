fe931dd5ccd000999a8d92ee15b5bbaeb521ded4
John Florian <john_florian@dart.biz>
2011-11-14 11:54:53 -0500

    New - bacula client: mdct-aos-master-f15
    While the host can be rebuilt entirely from a fresh Fedora install and
    the puppet manifest, the host still has the entire AOS conf.d pool to be
    concerned about.  Even that has a bare git repository on mdct-00fs that
    also gets bacula love, but if the "git push" via commit-resources (of
    the mdct-puppeteer-admin package) is failing for some reason, this
    backup would be the last line of defense.  So yes, a good thing.

--- modules/dart/manifests/classes/mdct-aos-master-f15.pp
+++ modules/dart/manifests/classes/mdct-aos-master-f15.pp
@@ -1,4 +1,9 @@
 # modules/dart/manifests/classes/mdct-aos-master-f15.pp
 
 class dart::mdct-aos-master-f15 inherits dart::aos_master_node {
+
+    $bacula_client_director_password = "hobe9yveB940mWreqoVzfTMIXfWurWi5ROughoJw7A39"
+    $bacula_client_director_monitor_password = "zdJxxdFhes9YxlJNhAHFOaSbneIy9N3FmlzTkA1wdowU"
+    include bacula::client
+
 }
