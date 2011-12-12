62a08ca95d3a72b9d6f5589fe7de3d5df6c6983a
John Florian <john_florian@dart.biz>
2011-12-12 13:52:00 -0500

    New - force plymouth to use details theme on all servers
    Servers don't need to look pretty during boot.  They are arguably more
    helpful to show details instead.

--- modules/dart/manifests/classes/abstract/server_node.pp
+++ modules/dart/manifests/classes/abstract/server_node.pp
@@ -2,4 +2,8 @@
 
 class dart::server_node inherits dart::base_node {
     include packages::net_tools
+
+    # Servers especially benefit from showing their boot details.
+    $plymouth_default_theme = "details"
+    include plymouth
 }
