7bb00d845415bdd3264c54c620134e100d37a2dc
John Florian <john_florian@dart.biz>
2011-12-22 08:13:28 -0500

    New - mail alias for puppet -> root
    This is to make sure puppet's mail gets delivered.  Without this
    puppet's mailbox grows quite large rather fast.

--- modules/dart/manifests/classes/abstract/puppet_server_node.pp
+++ modules/dart/manifests/classes/abstract/puppet_server_node.pp
@@ -1,13 +1,19 @@
 # modules/dart/manifests/classes/puppet_server_node.pp
 
 class dart::puppet_server_node inherits dart::server_node {
-    include packages::developer
-    include puppet::server
-    include yum-cron
 
-    mailalias { "root":
+    include 'packages::developer'
+    include 'puppet::server'
+    include 'yum-cron'
+
+    mailalias { 'puppet':
         ensure          => present,
-        recipient       => "john.florian@dart.biz",
+        recipient       => 'root',
+    }
+
+    mailalias { 'root':
+        ensure          => present,
+        recipient       => 'john.florian@dart.biz',
     }
 
 }
