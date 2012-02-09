41f8ce836c49c2bd046082f790906964b22f42a2
John Florian <john_florian@dart.biz>
2012-02-09 15:34:55 -0500

    New - picaps-backup-agent module

---
+++ modules/picaps-backup-agent/manifests/classes/agent.pp
@@ -0,0 +1,33 @@
+# modules/picaps-backup-agent/manifests/classes/agent.pp
+#
+# Synopsis:
+#       Configures a host as a picaps-backup-agent.
+#
+# Parameters:
+#       Name__________  Default_______  Description___________________________
+#
+#       NONE
+#
+# Requires:
+#       NONE
+#
+# Example usage:
+#
+#       include picaps-backup-agent
+
+class picaps-backup-agent {
+
+    include cron::daemon
+
+    package { 'picaps-backup-agent':
+        ensure  => installed,
+    }
+
+    cron::jobfile { 'picaps-backup-agent':
+        require => [
+            Package['picaps-backup-agent'],
+        ],
+        source  => 'puppet:///private-host/picaps-backup-agent/picaps-backup-agent.cron',
+    }
+
+}
