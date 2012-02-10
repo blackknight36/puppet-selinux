ef592e4a6b77262b711708e522a7bb07fb609e3c
John Florian <john_florian@dart.biz>
2012-02-10 08:06:08 -0500

    Change - ngic_server_node need tcp/8080 open for tomcat

--- modules/dart/manifests/classes/abstract/ngic_server_node.pp
+++ modules/dart/manifests/classes/abstract/ngic_server_node.pp
@@ -2,10 +2,15 @@
 
 class dart::ngic_server_node inherits dart::server_node {
 
+    include lokkit
     include postgresql::server
 
     package { [ 'tomcat', 'tomcat-admin-webapps', ]:
         ensure  => installed,
+    }
+
+    lokkit::tcp_port { 'tomcat':
+        port    => '8080',
     }
 
 }
