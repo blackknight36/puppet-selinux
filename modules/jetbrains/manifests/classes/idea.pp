1e06b22ec31fc69a01a134c7aa347b81900e4907
John Florian <john_florian@dart.biz>
2012-02-21 14:55:21 -0500

    Fix - module headers still as template

--- modules/jetbrains/manifests/classes/idea.pp
+++ modules/jetbrains/manifests/classes/idea.pp
@@ -6,22 +6,13 @@
 # Parameters:
 #       Name__________  Default_______  Description___________________________
 #
-#       name                            instance name
-#       ensure          present         instance is to be present/absent
+#       NONE
 #
 # Requires:
 #       NONE
-#       Class['REQ_MODULE::REQ_CLASS'] <= Use this notation for other resources
-#
-#       $MODULE_NAME_var1                       Abstract variable 1
-#       $MODULE_NAME_var2                       Abstract variable 2
-#       $MODULE_NAME_CONFIG_NAME_source         Source URI for the CONFIG_NAME file
 #
 # Example usage:
 #
-#       $MODULE_NAME_var1 = 'X_foo'
-#       $MODULE_NAME_var2 = 'X_bar'
-#       $MODULE_NAME_CONFIG_NAME_source = 'puppet:///private-host/CONFIG_NAME'
 #       include jetbrains::idea
 
 class jetbrains::idea {
