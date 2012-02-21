ce65e5b183d5168131c436c694f9efa160f16631
John Florian <john_florian@dart.biz>
2012-02-21 14:57:43 -0500

    New - open GWT ports on mdct-dev16

--- modules/dart/manifests/classes/mdct-dev16.pp
+++ modules/dart/manifests/classes/mdct-dev16.pp
@@ -1,8 +1,17 @@
 # modules/dart/manifests/classes/mdct-dev16.pp
 
 class dart::mdct-dev16 inherits dart::workstation_node {
+
     include packages::kde
     include yum-cron
+
+    lokkit::tcp_port { 'gwt-codeserver':
+        port    => '9997',
+    }
+
+    lokkit::tcp_port { 'gwt-debug':
+        port    => '8888',
+    }
 
     lokkit::tcp_port { 'postgres':
         port    => '5432',
