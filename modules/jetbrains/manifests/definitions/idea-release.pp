31b25c0971dcbd94bd8174edeb60dc908beab86a
John Florian <john_florian@dart.biz>
2012-04-02 11:57:36 -0400

    New - now supports ensure => absent

--- modules/jetbrains/manifests/definitions/idea-release.pp
+++ modules/jetbrains/manifests/definitions/idea-release.pp
@@ -20,12 +20,18 @@
 #       jetbrains::idea-release { 'latest':
 #           build   => "114.98",
 #       }
+#
+#       jetbrains::idea-release { 'ancient':
+#           build   => "1.23",
+#           ensure  => absent,
+#       }
 
 
-define jetbrains::idea-release ($build) {
+define jetbrains::idea-release ($build, $ensure='present') {
 
     file { "${idea_launchers}/${name}.desktop":
         content => template("jetbrains/idea/build.desktop"),
+        ensure  => "${ensure}",
         group   => 'root',
         mode    => '0644',
         owner   => 'root',
@@ -35,14 +41,32 @@ define jetbrains::idea-release ($build) {
         seluser => 'system_u',
     }
 
-    exec { "extract-${name}-idea":
-        command => "tar xzf /pub/jetbrains/${name}.tar.gz",
-        creates => "${idea_root}/idea-IU-${build}",
-        cwd     => "${idea_root}",
-        require => [
-            Class['autofs'],
-            File["${idea_root}"],
-        ],
+    case $ensure {
+
+        'present': {
+            exec { "extract-${name}-idea":
+                command => "tar xzf /pub/jetbrains/${name}.tar.gz",
+                creates => "${idea_root}/idea-IU-${build}",
+                cwd     => "${idea_root}",
+                require => [
+                    Class['autofs'],
+                    File["${idea_root}"],
+                ],
+            }
+        }
+
+        'absent': {
+            file { "${idea_root}/idea-IU-${build}":
+                ensure  => 'absent',
+                force   => true,
+                recurse => true,
+            }
+        }
+
+        default: {
+            fail('$ensure must be either "present" or "absent".')
+        }
+
     }
 
 }
