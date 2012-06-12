a67cbe23e458412cb220f5fdaace513747674bf2
root <root@mdct-puppet.dartcontainer.com>
2012-06-12 15:50:28 -0400

    Refactor - adjustment for puppet-2.7 compatibility
    This is an automated commit produced by tools/revise-resources-for-puppet-2.7.
    It is only one such commit in a series.  This revision specifically:
    
    - Renamed dart::dr_server_node to dart::abstract::dr_server_node.

--- modules/dart/manifests/mdct-dr-dev.pp
+++ modules/dart/manifests/mdct-dr-dev.pp
@@ -1,6 +1,6 @@
 # modules/dart/manifests/classes/mdct-ngic-dev.pp
 
-class dart::mdct-dr-dev inherits dart::dr_server_node {
+class dart::mdct-dr-dev inherits dart::abstract::dr_server_node {
 
     class { 'iptables':
         enabled => true,
--- modules/dart/manifests/mdct-dr.pp
+++ modules/dart/manifests/mdct-dr.pp
@@ -1,6 +1,6 @@
 # modules/dart/manifests/classes/mdct-ngic-dev.pp
 
-class dart::mdct-dr inherits dart::dr_server_node {
+class dart::mdct-dr inherits dart::abstract::dr_server_node {
 
     class { 'iptables':
         enabled => true,
