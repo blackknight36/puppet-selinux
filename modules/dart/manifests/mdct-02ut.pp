d66759027943d3ab0d8e4aa66c41670b220514ae
root <root@mdct-puppet.dartcontainer.com>
2012-06-12 15:50:28 -0400

    Refactor - adjustment for puppet-2.7 compatibility
    This is an automated commit produced by tools/revise-resources-for-puppet-2.7.
    It is only one such commit in a series.  This revision specifically:
    
    - Renamed dart::plant_utility_server_node to dart::abstract::plant_utility_server_node.

--- modules/dart/manifests/mdct-02ut.pp
+++ modules/dart/manifests/mdct-02ut.pp
@@ -1,5 +1,5 @@
 # modules/dart/manifests/classes/mdct-02ut.pp
 
-class dart::mdct-02ut inherits dart::plant_utility_server_node {
+class dart::mdct-02ut inherits dart::abstract::plant_utility_server_node {
 
 }
