82bc785a0a38edb0712dcee94c77a2e774057143
root <root@mdct-puppet.dartcontainer.com>
2012-06-12 15:50:28 -0400

    Refactor - adjustment for puppet-2.7 compatibility
    This is an automated commit produced by tools/revise-resources-for-puppet-2.7.
    It is only one such commit in a series.  This revision specifically:
    
    - Renamed dart::puppet_server_node to dart::abstract::puppet_server_node.

--- modules/dart/manifests/mdct-puppet.pp
+++ modules/dart/manifests/mdct-puppet.pp
@@ -1,6 +1,6 @@
 # modules/dart/manifests/classes/mdct-puppet.pp
 
-class dart::mdct-puppet inherits dart::puppet_server_node {
+class dart::mdct-puppet inherits dart::abstract::puppet_server_node {
     # passwords generated with bacula-password-generator package
     $bacula_client_director_password = "d730e38202fb2f6b8f8fb35045538a9f"
     $bacula_client_director_monitor_password = "db6696b5078c30697ae55a2788f0529d"
