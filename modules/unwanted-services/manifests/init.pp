6fb8f26fd79e1c3a0478560406abeb13c2d3bceb
John Florian <john_florian@dart.biz>
2012-07-27 09:49:03 -0400

    Drop - management of pcscd for Fedora 17+
    The in-line comments say it all.  Just not worth the time for a better
    fix.

--- modules/unwanted-services/manifests/init.pp
+++ modules/unwanted-services/manifests/init.pp
@@ -37,11 +37,17 @@ class unwanted-services {
         hasstatus       => true,
     }
 
-    service { "pcscd":
-        enable          => false,
-        ensure          => stopped,
-        hasrestart      => true,
-        hasstatus       => true,
+    # Puppet will endlessly change enable from true to false.  The systemd
+    # unit file has evolved since F16 and I don't have time to debug this
+    # right now.  Ignoring the service on F17 for now as this generates
+    # endless noise from puppet tagmail reports.
+    if $operatingsystem == 'Fedora' and $operatingsystemrelease < 17 {
+        service { "pcscd":
+            enable          => false,
+            ensure          => stopped,
+            hasrestart      => true,
+            hasstatus       => true,
+        }
     }
 
 }
