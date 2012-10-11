faa5f04db10d5744cd123bd6f8c23c01d75b14cb
John Florian <john_florian@dart.biz>
2012-10-11 09:49:45 -0400

    New - mirroring of RPM Fusion via mirrmaid
    This is intended to replace our motley collection of rsync scripts for
    mirror management.  Specifically, this commit obsoletes those scripts
    responsible for all aspects of maintaining the MDCT mirrors of RPM
    Fusion and covers both the "free" and "nonfree" YUM repositories for the
    following Fedora branches: Rawhide, 18 (development), 17 and 16.

--- modules/dart/manifests/mdct-00fs.pp
+++ modules/dart/manifests/mdct-00fs.pp
@@ -41,6 +41,10 @@ class dart::mdct-00fs {
         source => 'puppet:///private-host/mirrmaid/mirrmaid-fedora.conf',
     }
 
+    mirrmaid::config { 'mirrmaid-rpmfusion':
+        source => 'puppet:///private-host/mirrmaid/mirrmaid-rpmfusion.conf',
+    }
+
     mirrmaid::config { 'mirrmaid-testing':
         source => 'puppet:///private-host/mirrmaid/mirrmaid-testing.conf',
     }
@@ -48,6 +52,11 @@ class dart::mdct-00fs {
     cron::jobfile { 'mirrmaid-fedora':
         require => Class['mirrmaid'],
         source  => 'puppet:///private-host/mirrmaid/mirrmaid-fedora.cron',
+    }
+
+    cron::jobfile { 'mirrmaid-rpmfusion':
+        require => Class['mirrmaid'],
+        source  => 'puppet:///private-host/mirrmaid/mirrmaid-rpmfusion.cron',
     }
 
     include 'picaps-backup-agent'
