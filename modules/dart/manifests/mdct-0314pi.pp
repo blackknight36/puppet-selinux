d9d97009dc7b247a9fb07e35ada31b762dab89a4
Chris Kennedy <chris_kennedy@dart.biz>
2013-05-17 09:03:16 -0400

    Dallas PICAPS server instance

---
+++ modules/dart/manifests/mdct-0314pi.pp
@@ -0,0 +1,18 @@
+# modules/dart/manifests/mdct-0314pi.pp
+
+class dart::mdct-0314pi inherits dart::abstract::picaps_production_server_node {
+
+    #class { 'network':
+    #    network_manager => false,
+    #    domain => 'dartcontainer.com',
+    #    name_servers => ['10.1.0.98','10.1.0.99'],
+    #}
+
+    #network::interface { 'em1':
+    #    template => 'static',
+    #    ip_address  => '10.7.215.201',
+    #    netmask     => '255.255.252.0',
+    #    gateway     => '10.7.215.254',
+    #}
+
+}
