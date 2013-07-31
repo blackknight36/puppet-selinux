c0fa8a5d70abd3a5fda4dda1a6f2d2f202e0e220
Nate Nephew <nathan.nephew@dart.biz>
2013-07-31 14:04:46 -0400

    Updated to be similar to production PICAPS F18 build

--- modules/dart/manifests/mdct-04pt.pp
+++ modules/dart/manifests/mdct-04pt.pp
@@ -1,5 +1,5 @@
 # modules/dart/manifests/mdct-04pt.pp
 
-class dart::mdct-04pt inherits dart::abstract::picaps_test_node {
+class dart::mdct-04pt inherits dart::abstract::picaps_test_server_node {
 
 }
