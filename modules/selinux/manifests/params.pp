47bf9b49c2aa7ae8243989ab9fca88f2a41e7b5a
John Florian <john_florian@dart.biz>
2013-08-13 11:23:36 -0400

    Fix - audit2why not present on Fedora >= 18
    As of Fedora 18, the audit2why tool got moved from
    policycoreutils-python to policycoreutils-devel.  This commit also:
    
        - documents the $mode parameter
        - abstracts OS/release details to selinux::params
        - subscribes the config file to the packages for proper dependency
          ordering and event notification

---
+++ modules/selinux/manifests/params.pp
@@ -0,0 +1,39 @@
+# modules/selinux/manifests/params.pp
+#
+# Synopsis:
+#       Parameters for the selinux puppet module.
+
+
+class selinux::params {
+
+    case $::operatingsystem {
+        Fedora: {
+
+            if $::operatingsystemrelease < 18 {
+                $packages = [
+                    'libselinux',
+                    'libselinux-utils',
+                    'policycoreutils-python',   # provides audit2why
+                    'selinux-policy',
+                    'selinux-policy-targeted',
+                ]
+            } else {
+                $packages = [
+                    'libselinux',
+                    'libselinux-utils',
+                    'policycoreutils-python',
+                    'selinux-policy',
+                    'selinux-policy-devel',     # provides audit2why
+                    'selinux-policy-targeted',
+                ]
+            }
+
+        }
+
+        default: {
+            fail ("The selinux module is not yet supported on ${operatingsystem}.")
+        }
+
+    }
+
+}
