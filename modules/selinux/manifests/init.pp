980f23fc33ef1f127fd982dd5595cf4c794416f9
John Florian <john_florian@dart.biz>
2013-08-13 11:23:36 -0400

    Fix - audit2why not present on Fedora >= 18
    As of Fedora 18, the audit2why tool got moved from
    policycoreutils-python to policycoreutils-devel.  This commit also:
    
        - documents the $mode parameter
        - abstracts OS/release details to selinux::params
        - subscribes the config file to the packages for proper dependency
          ordering and event notification

--- modules/selinux/manifests/init.pp
+++ modules/selinux/manifests/init.pp
@@ -1,33 +1,36 @@
 # modules/selinux/manifests/init.pp
 #
 # Synopsis:
-#       Configures a host for SELinux.
+#       Configures SELinux on a host.
 #
 # Parameters:
-#       NONE
+#       Name__________  Notes_  Description___________________________
+#
+#       mode            1       One of: 'enforcing', 'permissive', or
+#                               'disabled'.
+#
+# Notes:
+#
+#       1. Default is 'enforcing'.
 #
 # Requires:
 #       NONE
 #
 # Usage Notes:
-# 	-Switching mode from enforcing or permissive to disabled requires reboot.
-# 	-Switching mode from disabled to enforcing or permissive requires reboot.
 #
-# Example Usage:
+#       - Switching mode from enforcing or permissive to disabled requires
+#       reboot.
 #
-#       include selinux
+#       - Switching mode from disabled to enforcing or permissive requires
+#       reboot.
 
 
 class selinux ($mode='enforcing') {
 
-    package { [
-        'libselinux',
-        'libselinux-utils',
-        'policycoreutils-python',
-        'selinux-policy',
-        'selinux-policy-targeted',
-        ]:
-	ensure	=> installed,
+    include 'selinux::params'
+
+    package { $selinux::params::packages:
+        ensure  => installed,
     }
 
     File {
@@ -37,6 +40,7 @@ class selinux ($mode='enforcing') {
         seluser     => 'system_u',
         selrole     => 'object_r',
         seltype     => 'selinux_config_t',
+        subscribe   => Package[$selinux::params::packages],
     }
 
     file { '/etc/selinux/config':
