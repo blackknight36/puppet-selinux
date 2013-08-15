b425c2ed65f7368740d1baa020de2d73d5f79861
John Florian <john_florian@dart.biz>
2013-08-15 14:41:21 -0400

    Fix - SSH host keys unmanaged on AOS Master(s)
    It is not possible to implement a new set of SSH host keys for the AOS
    Master (such as during a fresh rebuild) because the AOS nodes must use
    public-key connections to retrieve their resources.  They could retrieve
    new host keys, but only from an already trusted AOS Master.

--- modules/dart/manifests/abstract/aos_master_node.pp
+++ modules/dart/manifests/abstract/aos_master_node.pp
@@ -21,7 +21,7 @@ class dart::abstract::aos_master_node inherits dart::abstract::server_node {
     }
 
     class { 'vsftpd':
-        allow_use_nfs      => true,
+        allow_use_nfs   => true,
     }
 
     # vsftpd will deny clients access to /pub/mdct-aos-flash/ so we must rbind
@@ -44,9 +44,34 @@ class dart::abstract::aos_master_node inherits dart::abstract::server_node {
         source  => 'puppet:///private-host/lokkit/blocks',
     }
 
-    mailalias { "root":
-        ensure          => present,
-        recipient       => "john.florian@dart.biz",
+    mailalias { 'root':
+        ensure      => present,
+        recipient   => 'john.florian@dart.biz',
+    }
+
+    # All AOS Masters require the same SSH host keys lest the nodes not
+    # implicitly trust them.  This is especially important when the AOS Master
+    # is being replaced with a fresh build and there's a transitional period.
+    file { '/etc/ssh/ssh_host_rsa_key':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'sshd_key_t',
+        notify  => Class['openssh-server'],
+        source  => 'puppet:///private-domain/ssh/aos-master-host_rsa_key',
+    }
+
+    file { '/etc/ssh/ssh_host_rsa_key.pub':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'etc_t',
+        notify  => Class['openssh-server'],
+        source  => 'puppet:///private-domain/ssh/aos-master-host_rsa_key.pub',
     }
 
 }
