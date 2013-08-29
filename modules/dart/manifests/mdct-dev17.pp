1a0904c7d40b2b73f02165d760d84a6372bfadb8
John Florian <john_florian@dart.biz>
2013-08-29 11:19:50 -0400

    New - open TCP/8080 on Collin's workstation
    This per his request for testing some Tomcat work.

--- modules/dart/manifests/mdct-dev17.pp
+++ modules/dart/manifests/mdct-dev17.pp
@@ -11,4 +11,9 @@ class dart::mdct-dev17 inherits dart::abstract::workstation_node {
     include 'dart::subsys::yum_cron'
     include 'jetbrains::idea'
 
+    iptables::tcp_port {
+        'tomcat': port => '8080';
+        };
+    }
+
 }
