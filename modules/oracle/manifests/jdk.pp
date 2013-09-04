eab76ff26f870edfec93acf78eab3962e60a39e1
John Florian <john_florian@dart.biz>
2013-09-04 15:52:39 -0400

    New - $arch parameter for oracle::jdk
    Users may now pass 'i586' for 32-bit JDK installations or 'x86'
    (default) for 64-bit.

--- modules/oracle/manifests/jdk.pp
+++ modules/oracle/manifests/jdk.pp
@@ -14,15 +14,17 @@
 #
 #       update                  JDK update number (e.g., '25')
 #
+#       arch                    platform architecure (i.e., 'x64' or 'i586')
+#
 # Notes:
 #
 #       1. Default is 'present'.
 
 
-define oracle::jdk ($ensure='present', $version, $update) {
+define oracle::jdk ($ensure='present', $version, $update, $arch='x64') {
 
     exec { "install oracle $name":
-        command => "rpm -i --force http://mdct-00fs.dartcontainer.com/ftp/pub/oracle/jdk-${version}u${update}-linux-x64.rpm",
+        command => "rpm -i --force http://mdct-00fs.dartcontainer.com/ftp/pub/oracle/jdk-${version}u${update}-linux-${arch}.rpm",
         unless  => "rpm -q jdk-1.${version}.0_${update}-fcs",
     }
 
