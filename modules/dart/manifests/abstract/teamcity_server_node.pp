516e5798013094f708d1dfcc76bec032531ffb52
John Florian <john_florian@dart.biz>
2013-09-10 11:32:55 -0400

    New - dart::abstract::teamcity_server_node class
    This class provides (or intends to) everything needed to establish
    a typical TeamCity Server for Dart use.

---
+++ modules/dart/manifests/abstract/teamcity_server_node.pp
@@ -0,0 +1,33 @@
+# modules/dart/manifests/abstract/teamcity_server_node.pp
+#
+# Synopsis:
+#       Typical TeamCity Server for Dart use.
+
+
+class dart::abstract::teamcity_server_node inherits dart::abstract::guarded_server_node {
+
+    include 'autofs'
+    include 'dart::subsys::yum_cron'
+
+    oracle::jdk { 'for TeamCity Server':
+        ensure  => 'latest',
+        # Oracle's architecture labeling is non-standard; so we adapt here.
+        arch    => $architecture ? {
+            'i386'      => 'i586',
+            'x86_64'    => 'x64',
+        },
+    }
+
+    include 'packages::developer'
+    include 'puppet::client'
+
+    # This package allows optimal performance in production environments.
+    package { 'tomcat-native':
+        ensure  => installed,
+    }
+
+    jetbrains::teamcity_release { 'TeamCity-8.0.3':
+        build   => '8.0.3',
+    }
+
+}
