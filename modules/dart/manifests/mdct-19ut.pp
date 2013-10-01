970a6338bf49d5e9767289b36309bb6c6460e26c
Chris Kennedy <chris_kennedy@dart.biz>
2013-10-01 16:51:58 -0400

    Introduced mdct-19ut for Information Display system in Plant City

---
+++ modules/dart/manifests/mdct-19ut.pp
@@ -0,0 +1,11 @@
+# modules/dart/manifests/mdct-19ut.pp
+#
+# Synopsis:
+#       General purpose utility server for Plant City
+#
+# Contact:
+#       John Florian
+
+class dart::mdct-19ut inherits dart::abstract::plant_utility_server_node {
+
+}
