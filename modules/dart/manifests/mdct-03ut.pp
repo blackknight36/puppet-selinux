7861ba92254cc387655ce08bac98b17651e3063e
Chris Kennedy <chris_kennedy@dart.biz>
2013-10-01 16:53:09 -0400

    Introduced mdct-03ut for Information Display system in Lithonia

---
+++ modules/dart/manifests/mdct-03ut.pp
@@ -0,0 +1,11 @@
+# modules/dart/manifests/mdct-03ut.pp
+#
+# Synopsis:
+#       General purpose utility server for Lithonia
+#
+# Contact:
+#       John Florian
+
+class dart::mdct-03ut inherits dart::abstract::plant_utility_server_node {
+
+}
