6578fa042671b08f9712b96eb3a369dbe0cedfed
John Florian <john_florian@dart.biz>
2013-10-14 11:26:40 -0400

    New - koji::database class
    This new class does most of the work necessary to configure a host to
    provide the Koji database.  Additional manual work is required to fully
    prepare the database cluster.  See notes within the class for more
    details.

---
+++ modules/koji/manifests/database.pp
@@ -0,0 +1,29 @@
+# modules/koji/manifests/database.pp
+#
+# Synopsis:
+#       Configures a host to provide the koji database.
+#
+# Parameters:
+#       Name__________  Notes_  Description_______________________________
+#
+#       config_source   1       configuration source for the Koji database
+#
+# Notes:
+#       This class presently assumes a PostgreSQL database.  Additional
+#       manual work is required to create the database, import the schema and
+#       provide the initial population.  See the following for more details:
+#       https://fedoraproject.org/wiki/Koji/ServerHowTo#PostgreSQL_Server
+
+
+class koji::database ( $config_source ) {
+
+    include 'postgresql::server'
+
+    # NB: This config is hardly needed.  Presently, the only requirement would
+    # be to adjust listen_addresses and even that is only needed when the
+    # Koji-Hub runs a host distinct from that providing the database.
+    postgresql::config { 'postgresql.conf':
+        source  => $config_source,
+    }
+
+}
