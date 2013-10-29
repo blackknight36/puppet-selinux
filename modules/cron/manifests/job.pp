a745cb0eff9813b99b85b090f15f5a217a7ca29f
John Florian <john_florian@dart.biz>
2013-10-29 15:44:28 -0400

    Change - convert to RDoc format

--- modules/cron/manifests/job.pp
+++ modules/cron/manifests/job.pp
@@ -1,33 +1,58 @@
 # modules/cron/manifests/job.pp
 #
-# Synopsis:
-#       Installs a single cron job.
+# == Define: cron::job
 #
-# Parameters:
-#       Name__________  Default_______  Description___________________________
-#       name                            used as /etc/cron.d/$name
-#       command                         command to be executed
-#       ensure          present         absent/present
-#       user            root            user to run job under
-#       minute          *               0-59
-#       hour            *               0-23
-#       dom             *               1-31; day of the month
-#       month           *               1-12 or use names
-#       dow             *               0-7 or use names; day of the week
-#       mailto                          results of cron job are sent here
-#       path            /sbin:/bin:/usr/sbin:/usr/bin
+# Installs a single job configuration for cron.
 #
-# Requires:
-#       Class['cron::daemon']
+# === Parameters
 #
-# Example usage:
+# [*namevar*]
+#   An arbitrary identifier for the job instance.  Results in a cron job file
+#   named "/etc/cron.d/$name".
 #
-#       include cron::daemon
+# [*ensure*]
+#   Instance is to be 'present' (default) or 'absent'.
 #
-#       cron::job { 'example':
-#           minute  => '*/3',
-#           command => 'date >> /tmp/crontest',
-#       }
+# [*command*]
+#   Command to be executed by cron.
+#
+# [*user*]
+#   Identity that cron should should assume when running the command.
+#   Defaults to "root".
+#
+# [*minute*]
+#   Valid: 0-59, ranges via hyphen, lists via comma or steps via slashes or no
+#   restriction via star.  Defaults to "*".
+#
+# [*hour*]
+#   Valid: 0-23, ranges via hyphen, lists via comma or steps via slashes or no
+#   restriction via star.  Defaults to "*".
+#
+# [*dom*]
+#   Valid: 1-31, ranges via hyphen, lists via comma or steps via slashes or no
+#   restriction via star.  Defaults to "*".
+#
+# [*month*]
+#   Valid: 1-12, names (e.g., "Feb"), ranges via hyphen, lists via comma or
+#   steps via slashes or no restriction via star.  Defaults to "*".
+#
+# [*dow*]
+#   Valid: 0-7, names (e.g., "Mon"), ranges via hyphen, lists via comma or
+#   steps via slashes or no restriction via star.  Defaults to "*".  Note that
+#   either 0 or 7 may be used to indicate Sunday.
+#
+# [*mailto*]
+#   Any stdout/stderr will be sent here.  May be a user name for localhost
+#   mail or a fully qualified email address.  Defaults to "root".
+#
+# [*path*]
+#   Executable search path to be used in environment while running command.
+#   Defaults to "/sbin:/bin:/usr/sbin:/usr/bin".
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
 
 define cron::job ($command,
                   $ensure='present',
