42227b9a02d0191b0ea1da6834079700e037da4b
John Florian <john_florian@dart.biz>
2013-10-31 17:42:37 -0400

    Change - migrate ckennedy's /mnt-local to /mnt
    This leverages the new autofs::mount_point and autofs::map_entry
    features so that my personal mounts can also reside under /mnt now.

--- modules/dart/manifests/mdct-dev6-test.pp
+++ modules/dart/manifests/mdct-dev6-test.pp
@@ -30,66 +30,129 @@ class dart::mdct-dev6-test inherits dart::abstract::workstation_node {
 
     $SUFFIX=".orig-${operatingsystem}${operatingsystemrelease}"
 
+    autofs::map_entry { '/mnt/storage':
+        mount   => '/mnt',
+        key     => 'storage',
+        options => '-fstype=ext3,rw',
+        remote  => ':/dev/data/storage',
+    }
+
+    autofs::map_entry { '/mnt/mdct-dev12-aud':
+        mount   => '/mnt',
+        key     => 'mdct-dev12-aud',
+        options => '-soft',
+        remote  => 'mdct-dev12.dartcontainer.com:/Music',
+    }
+
+    autofs::map_entry { '/mnt/f15root':
+        mount   => '/mnt',
+        key     => 'f15root',
+        options => '-fstype=ext4,ro',
+        remote  => ':/dev/mdctdev6_2010/f15root/',
+    }
+
+    autofs::map_entry { '/mnt/mas9965':
+        mount   => '/mnt',
+        key     => 'mas9965',
+        options => '-fstype=cifs,rw,credentials=/etc/.credentials/mas9965.cred',
+        remote  => '://mas9965.dartcontainer.com/c\$',
+    }
+
+    autofs::map_entry { '/mnt/files-2k1-prodmondata':
+        mount   => '/mnt',
+        key     => 'files-2k1-prodmondata',
+        options => '-fstype=cifs,rw,uid=d19749,gid=d19749,credentials=/etc/.credentials/d19749.cred',
+        remote  => '://files-2k1/prodmondata',
+    }
+
+    autofs::map_entry { '/mnt/files-2k1-media':
+        mount   => '/mnt',
+        key     => 'files-2k1-media',
+        options => '-fstype=cifs,rw,uid=d19749,gid=d19749,credentials=/etc/.credentials/d19749.cred',
+        remote  => '://files-2k1/media',
+    }
+
+    autofs::map_entry { '/mnt/files-2k1-sharedata':
+        mount   => '/mnt',
+        key     => 'files-2k1-sharedata',
+        options => '-fstype=cifs,rw,uid=d19749,gid=d19749,credentials=/etc/.credentials/d19749.cred',
+        remote  => '://files-2k1/ShareData',
+    }
+
+    autofs::map_entry { '/mnt/files-2k1-eng':
+        mount   => '/mnt',
+        key     => 'files-2k1-eng',
+        options => '-fstype=cifs,rw,uid=d19749,gid=d19749,credentials=/etc/.credentials/d19749.cred',
+        remote  => '://files-2k1/Eng',
+    }
+
+    autofs::map_entry { '/mnt/d19749':
+        mount   => '/mnt',
+        key     => 'd19749',
+        options => '-fstype=cifs,rw,uid=d19749,gid=d19749,credentials=/etc/.credentials/d19749.cred',
+        remote  => '://mas-fs02/users/d19749',
+    }
+
     file { "/etc/.credentials":
-        ensure  => "/mnt-local/storage/etc/.credentials",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/etc/.credentials",
     }
 
     file { "/storage":
-        ensure  => "/mnt-local/storage/",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/",
     }
 
     file { "/cpk":
-        ensure  => "/mnt-local/storage/pub/",
-        require => Service["autofs"],
+        ensure  => link,
+        taret   => "/mnt/storage/pub/",
     }
 
     file { "/dist":
-        ensure  => "/mnt-local/storage/dist/",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/dist/",
     }
 
     file { "/etc/init.d/picaps":
-        ensure  => "/mnt-local/storage/dist/resource/init.d/picaps",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/dist/resource/init.d/picaps",
     }
 
     file { "/etc/sysconfig/picaps":
-        ensure  => "/mnt-local/storage/dist/resource/init.d/sysconfig",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/dist/resource/init.d/sysconfig",
     }
 
     file { "/usr/local/idea":
-        ensure  => "/mnt-local/storage/usr/local/idea",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/usr/local/idea",
     }
 
     file { "/usr/local/yjp":
-        ensure  => "/mnt-local/storage/usr/local/yjp",
-        require => Service["autofs"],
+        ensure  => link,
+        target  => "/mnt/storage/usr/local/yjp",
     }
 
     dart::util::replace_original_with_symlink_to_alternate { "/etc/libvirt":
-        alternate   => "/mnt-local/storage/etc/libvirt",
+        alternate   => "/mnt/storage/etc/libvirt",
         backup      => "/etc/libvirt$SUFFIX",
         original    => "/etc/libvirt",
         # TODO: libvirt needs to be a formal service and treated here like
         # mysql WRT before/require attrs
-        require         => Package["libvirt"],
+        require     => Package["libvirt"],
     }
 
     dart::util::replace_original_with_symlink_to_alternate { "/var/lib/libvirt":
-        alternate   => "/mnt-local/storage/var/lib/libvirt",
+        alternate   => "/mnt/storage/var/lib/libvirt",
         backup      => "/var/lib/libvirt$SUFFIX",
         original    => "/var/lib/libvirt",
         # TODO: libvirt needs to be a formal service and treated here like
         # mysql WRT before/require attrs
-        require         => Package["libvirt"],
+        require     => Package["libvirt"],
     }
 
     dart::util::replace_original_with_symlink_to_alternate { "/var/lib/mysql":
-        alternate   => "/mnt-local/storage/var/lib/mysql",
+        alternate   => "/mnt/storage/var/lib/mysql",
         backup      => "/var/lib/mysql$SUFFIX",
         original    => "/var/lib/mysql",
         before      => Service["mysqld"],
