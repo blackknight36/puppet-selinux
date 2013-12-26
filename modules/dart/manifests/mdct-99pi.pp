21e5cbc359304d9a25e6afda403a886bc3dd4ced
Chris Kennedy <chris_kennedy@dart.biz>
2013-12-26 10:01:09 -0500

    Commissioned mdct-99pi on R720 hardware

---
+++ modules/dart/manifests/mdct-99pi.pp
@@ -0,0 +1,24 @@
+# modules/dart/manifests/mdct-99pi.pp
+#
+# Synopsis:
+#       PICAPS test server for TestPlant99 
+#
+# Contact:
+#       Chris Kennedy
+
+class dart::mdct-99pi inherits dart::abstract::picaps_production_server_node {
+
+    #class { 'network':
+    #    network_manager => false,
+    #    domain          => 'dartcontainer.com',
+    #    name_servers    => ['10.1.0.98','10.1.0.99'],
+    #}
+
+    #network::interface { 'em1':
+    #    template    => 'static',
+    #    ip_address  => '10.1.192.149',
+    #    netmask     => '255.255.0.0',
+    #    gateway     => '10.1.0.25',
+    #}
+
+}
