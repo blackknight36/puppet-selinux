0e752f3f1f877b3d82c690771a90b1322f14b21e
John Florian <john_florian@dart.biz>
2013-12-26 10:41:25 -0500

    Change - more missed revisions for 0310pi

--- modules/dart/manifests/mdct-0310pi.pp
+++ modules/dart/manifests/mdct-0310pi.pp
@@ -11,14 +11,14 @@ class dart::mdct-0310pi inherits dart::abstract::picaps_production_server_node {
     #class { 'network':
     #    network_manager => false,
     #    domain => 'dartcontainer.com',
-    #    name_servers => ['10.1.0.98','10.1.0.99'],
+    #    name_servers => ['10.101.1.114','10.1.0.98','10.1.0.99'],
     #}
 
     #network::interface { 'em1':
     #    template => 'static',
-    #    ip_address  => '10.7.87.3',
+    #    ip_address  => '10.31.55.3',
     #    netmask     => '255.255.252.0',
-    #    gateway     => '10.7.87.254',
+    #    gateway     => '10.31.55.254',
     #}
 
 }
