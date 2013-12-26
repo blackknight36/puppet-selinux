674ce6a5369bdf041ca2d82e2105c3e8c308947c
John Florian <john_florian@dart.biz>
2013-12-26 14:12:25 -0500

    Change - major authconfig cleanup
    - drop support for Fedora 13 and older; can't support anyway due to
      other requirements (concat module, pluginsync, etc.)
    - abstract OS dependencies into new authconfig::params class
    - make more compact through use of resource type defaults
    - simplify/improve and maybe even correct dependency chain/event flows
    - manage SE Linux context

--- modules/authconfig/manifests/init.pp
+++ modules/authconfig/manifests/init.pp
@@ -1,120 +1,88 @@
 # modules/authconfig/manifests/init.pp
+#
+# == Class: authconfig
+#
+# Configures user authentication on a host.
+#
+# === Parameters
+#
+# NONE
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
 
 class authconfig {
 
     # Stage => early
 
-    package { 'authconfig':
-         ensure => installed
+    include 'authconfig::params'
+
+    # nscd (and it's dependencies) have been replaced by sssd, but oddly
+    # the packaging may not obsolete it, so we do so here.
+    yum::remove { 'nscd': }
+
+    package { $authconfig::params::packages:
+        ensure  => installed,
+        before  => Exec['authconfig'],
+        require => Yum::Remove['nscd'],
+        notify  => Service[$authconfig::params::service_name],
     }
 
-    package { 'pam':
-        ensure  => installed,
+    File {
+        owner       => 'root',
+        group       => 'root',
+        mode        => '0644',
+        seluser     => 'system_u',
+        selrole     => 'object_r',
+        seltype     => 'etc_t',
+        before      => Service[$authconfig::params::service_name],
+        notify      => Service[$authconfig::params::service_name],
+        subscribe   => Package[$authconfig::params::packages],
     }
 
     file { '/etc/pam.d/system-auth-ac':
         before  => Exec['authconfig'],
-        group   => 'root',
-        mode    => 644,
-        owner   => 'root',
-        require => Package['pam'],
         source  => [
-            "puppet:///modules/authconfig/system-auth-ac.$operatingsystem.$operatingsystemrelease",
+            "puppet:///modules/authconfig/system-auth-ac.${operatingsystem}.${operatingsystemrelease}",
             'puppet:///modules/authconfig/system-auth-ac',
         ],
     }
 
-    if  $operatingsystem == 'Fedora' and
-        $operatingsystemrelease == "Rawhide" or
-        $operatingsystemrelease >= 13
-    {
+    file { '/etc/pam.d/password-auth-ac':
+        before  => Exec['authconfig'],
+        source  => 'puppet:///modules/authconfig/password-auth-ac',
+    }
 
-        package { [ 'krb5-libs', 'pam_krb5' ]:
-            ensure  => installed,
-        }
+    # Fedora requires encryption to a directory server if it is to be used for
+    # authentication.  Since MDCT's LDAP server doesn't provide encryption,
+    # authentication is configured for Kerberos against the Active Directory
+    # while all other user account information continues to be obtained from
+    # LDAP.
+    exec { 'authconfig':
+        before  => File['/etc/sssd/sssd.conf'],
+        command => 'authconfig --enableldap --disableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --enablesssd --disablesssdauth --enablekrb5 --krb5realm=DARTCONTAINER.COM --krb5kdc=dartcontainer.com --enablecachecreds --updateall --disablefingerprint',
+        unless => 'grep -q "^ *USEKERBEROS=yes" /etc/sysconfig/authconfig'
+    }
 
-        # As of Fedora 13, nscd (and it's dependent nss_ldap) is being
-        # replaced by sssd, but oddly the packaging doesn't obsolete it, so we
-        # do so here.
-        package { 'nss_ldap':
-            ensure  => absent,
-        }
+    # According to authconfig(8), authconfig does NOT configure the domain in
+    # sssd.conf; it must be done manually.  Hence a starter sssd.conf is
+    # provided here.
+    file { '/etc/sssd/sssd.conf':
+        mode    => '0600',
+        seltype => "$authconfig::params::sssd_seltype",
+        source  => [
+            "puppet:///private-host/authconfig/$authconfig::params::sssd_conf",
+            "puppet:///modules/authconfig/$authconfig::params::sssd_conf",
+        ],
+    }
 
-        package { 'nscd':
-            ensure  => absent,
-            require => Package['nss_ldap'],
-        }
-
-        package { 'sssd':
-            ensure  => installed,
-            require => Package['nscd'],
-        }
-
-        file { '/etc/pam.d/password-auth-ac':
-            group   => 'root',
-            mode    => '0644',
-            owner   => 'root',
-            require => Package['pam'],
-            source  => [
-                'puppet:///modules/authconfig/password-auth-ac',
-            ],
-        }
-
-        # As of Fedora 13, encryption is required to a directory server if it
-        # is to be used for authentication.  Since MDCT's LDAP server doesn't
-        # provide encryption, authentication is configured for Kerberos
-        # against the Active Directory while all other user account
-        # information continues to be obtained from LDAP.
-        exec { 'authconfig':
-            before  => File['/etc/sssd/sssd.conf'],
-            command => 'authconfig --enableldap --disableldapauth --ldapserver="ldap://10.1.192.106" --ldapbasedn="dc=dartcontainer,dc=com" --disableldaptls --enablesssd --disablesssdauth --enablekrb5 --krb5realm=DARTCONTAINER.COM --krb5kdc=dartcontainer.com --enablecachecreds --updateall --disablefingerprint',
-            require => [
-                File['/etc/pam.d/password-auth-ac'],
-                File['/etc/pam.d/system-auth-ac'],
-                Package['authconfig'],
-                Package['krb5-libs'],
-                Package['nscd'],
-                Package['pam_krb5'],
-                Package['sssd'],
-            ],
-            unless => 'grep -q "^ *USEKERBEROS=yes" /etc/sysconfig/authconfig'
-        }
-
-        # According to F13's authconfig(8), authconfig does NOT configure the
-        # domain in sssd.conf; it must be done manually.  Hence a starter
-        # sssd.conf is provided here.
-        if  $operatingsystemrelease == "Rawhide" or
-            $operatingsystemrelease >= 15
-        {
-            $sssd_conf = 'sssd.conf.Fedora.15+'
-        } else {
-            $sssd_conf = 'sssd.conf'
-        }
-        file { '/etc/sssd/sssd.conf':
-            group   => 'root',
-            mode    => '0600',
-            owner   => 'root',
-            require => Package['sssd'],
-            source  => [
-                "puppet:///private-host/authconfig/$sssd_conf",
-                "puppet:///modules/authconfig/$sssd_conf",
-            ],
-        }
-
-        service { 'sssd':
-            enable          => true,
-            ensure          => running,
-            hasrestart      => true,
-            hasstatus       => true,
-            require         => [
-                Exec['authconfig'],
-                Package['sssd'],
-            ],
-            subscribe       => [
-                File['/etc/sssd/sssd.conf'],
-            ],
-        }
-
+    service { $authconfig::params::service_name:
+        enable      => true,
+        ensure      => running,
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
 }
---
+++ modules/authconfig/manifests/params.pp
@@ -0,0 +1,50 @@
+# modules/authconfig/manifests/params.pp
+#
+# == Class: authconfig::params
+#
+# Parameters for the authconfig puppet module.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+class authconfig::params {
+
+    case $::operatingsystem {
+        Fedora: {
+
+            $packages = [
+                'authconfig',
+                'krb5-libs',
+                'pam',
+                'pam_krb5',
+                'sssd',
+            ]
+            $service_name = 'sssd'
+
+
+            if  $operatingsystemrelease == "Rawhide" or
+                $operatingsystemrelease >= 15
+            {
+                $sssd_conf = 'sssd.conf.Fedora.15+'
+            } else {
+                $sssd_conf = 'sssd.conf'
+            }
+
+            if  $operatingsystemrelease == "Rawhide" or
+                $operatingsystemrelease >= 18
+            {
+                $sssd_seltype = 'sssd_conf_t'
+            } else {
+                $sssd_seltype = 'etc_t'
+            }
+
+        }
+        default: {
+            fail ("The authconfig module is not yet supported on ${::operatingsystem}.")
+        }
+
+    }
+
+}
