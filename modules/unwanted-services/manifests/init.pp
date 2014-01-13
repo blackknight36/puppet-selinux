fb04ecac5c3b9af990d76d1a4c1731daaef5ce19
John Florian <john_florian@dart.biz>
2014-01-13 09:29:51 -0500

    Janitorial - general cleanup

--- modules/unwanted-services/manifests/init.pp
+++ modules/unwanted-services/manifests/init.pp
@@ -1,62 +1,75 @@
 # modules/unwanted-services/manifests/init.pp
+#
+# == Class: unwanted-services
+#
+# Stops and disables several services that are typically of no value.
+#
+# === Parameters
+#
+# NONE
+#
+# === Authors
+#
+#   John Florian <jflorian@doubledog.org>
+
 
 class unwanted-services {
 
-    service { "firstboot":
-        enable          => false,
-        ensure          => stopped,
-        hasrestart      => true,
-        hasstatus       => true,
+    service { 'firstboot':
+        enable      => false,
+        ensure      => stopped,
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
-    service { "iscsi":
-        enable          => false,
-        ensure          => stopped,
-        hasrestart      => true,
-        hasstatus       => true,
+    service { 'iscsi':
+        enable      => false,
+        ensure      => stopped,
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
-    service { "iscsid":
-        enable          => false,
-        ensure          => stopped,
-        hasrestart      => true,
-        hasstatus       => true,
+    service { 'iscsid':
+        enable      => false,
+        ensure      => stopped,
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
-    service { "livesys":
-        enable          => false,
-        #ensure         => stopped,     # there is nothing to stop
-        hasrestart      => true,
-        hasstatus       => true,
+    service { 'livesys':
+        enable      => false,
+        #ensure     => stopped,     # there is nothing to stop
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
-    service { "livesys-late":
-        enable          => false,
-        #ensure         => stopped,     # there is nothing to stop
-        hasrestart      => true,
-        hasstatus       => true,
+    service { 'livesys-late':
+        enable      => false,
+        #ensure     => stopped,     # there is nothing to stop
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
-    service { "ModemManager":
-        enable          => false,
-        ensure          => stopped,
-        hasrestart      => true,
-        hasstatus       => true,
+    service { 'ModemManager':
+        enable      => false,
+        ensure      => stopped,
+        hasrestart  => true,
+        hasstatus   => true,
     }
 
     # Puppet will endlessly change enable from true to false.  The systemd
     # unit file has evolved since F16 and I don't have time to debug this
     # right now.  Ignoring the service on F17 for now as this generates
     # endless noise from puppet tagmail reports.
-    if  $operatingsystem == 'Fedora' and
-        $operatingsystemrelease != 'Rawhide' and
-        $operatingsystemrelease < 17
+    if  $::operatingsystem == 'Fedora' and
+        $::operatingsystemrelease != 'Rawhide' and
+        $::operatingsystemrelease < 17
     {
-        service { "pcscd":
-            enable          => false,
-            ensure          => stopped,
-            hasrestart      => true,
-            hasstatus       => true,
+        service { 'pcscd':
+            enable      => false,
+            ensure      => stopped,
+            hasrestart  => true,
+            hasstatus   => true,
         }
     }
 
