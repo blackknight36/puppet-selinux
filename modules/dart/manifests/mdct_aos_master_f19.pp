e0d384aeb58e25b66f756e69d75fd4d2f7b62ce9
John Florian <john_florian@dart.biz>
2014-01-16 09:56:20 -0500

    Refactor - adjustment for puppet-3 compatibility
    This is an automated commit produced by tools/revise-resources-for-puppet-3.
    It is only one such commit in a series.  This revision specifically:
    
    - Renamed "dart::mdct-aos-master-f19" to "dart::mdct_aos_master_f19".

--- modules/dart/manifests/mdct_aos_master_f19.pp
+++ modules/dart/manifests/mdct_aos_master_f19.pp
@@ -6,7 +6,7 @@
 # Contact:
 #       John Florian
 
-class dart::mdct-aos-master-f19 inherits dart::abstract::aos_master_node {
+class dart::mdct_aos_master_f19 inherits dart::abstract::aos_master_node {
 
     class { 'bacula::client':
         dir_passwd  => 'hobe9yveB940mWreqoVzfTMIXfWurWi5ROughoJw7A39',
