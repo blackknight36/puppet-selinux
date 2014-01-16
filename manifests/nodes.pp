a9ea0987d9c6dfa91b254b7d2ffba4ebdbf01877
John Florian <john_florian@dart.biz>
2014-01-16 09:56:19 -0500

    Refactor - adjustment for puppet-3 compatibility
    This is an automated commit produced by tools/revise-resources-for-puppet-3.
    It is only one such commit in a series.  This revision specifically:
    
    - Renamed "dart::mdct-dev10-srvr" to "dart::mdct_dev10_srvr".

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -74,6 +74,6 @@ node 'mole-dev.dartcontainer.com'               { include 'dart::mole_dev' }
 node 'mole.dartcontainer.com'                   { include 'dart::mole' }
 node 'tc-util.dartcontainer.com'                { include 'dart::tc_util' }
 
-node /^mdct-dev10-.*\.dartcontainer\.com$/      { include 'dart::mdct-dev10-srvr' }
+node /^mdct-dev10-.*\.dartcontainer\.com$/      { include 'dart::mdct_dev10_srvr' }
 node /^mdct-ovirt-node-.*\.dartcontainer\.com$/ { include 'dart::mdct_ovirt_node' }
 node /^mdct-puppet.*\.dartcontainer\.com$/      { include 'dart::mdct_puppet' }
--- modules/dart/manifests/mdct_dev10_srvr.pp
+++ modules/dart/manifests/mdct_dev10_srvr.pp
@@ -6,7 +6,7 @@
 # Contact:
 #       Levi Harper
 
-class dart::mdct-dev10-srvr inherits dart::abstract::guarded_server_node {
+class dart::mdct_dev10_srvr inherits dart::abstract::guarded_server_node {
 
     class { 'puppet::client':
     }
