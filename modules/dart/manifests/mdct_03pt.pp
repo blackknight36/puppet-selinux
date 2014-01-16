78349e1400bbe510082022b6f79d3de67bc2dbeb
John Florian <john_florian@dart.biz>
2014-01-16 09:56:26 -0500

    Refactor - adjustment for puppet-3 compatibility
    This is an automated commit produced by tools/revise-resources-for-puppet-3.
    It is only one such commit in a series.  This revision specifically:
    
    - Renamed "dart::mdct-03pt" to "dart::mdct_03pt".

--- modules/dart/manifests/mdct_03pt.pp
+++ modules/dart/manifests/mdct_03pt.pp
@@ -6,6 +6,6 @@
 # Contact:
 #       Nathan Nephew
 
-class dart::mdct-03pt inherits dart::abstract::picaps_test_server_node {
+class dart::mdct_03pt inherits dart::abstract::picaps_test_server_node {
 
 }
