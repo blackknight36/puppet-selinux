4009d82470e1f3ad8c130d39a18aa663aec60a95
John Florian <john_florian@dart.biz>
2014-02-17 11:58:34 -0500

    Fix - puppet client service name instability
    This is a continuation of ba60350e.

--- modules/puppet/manifests/params.pp
+++ modules/puppet/manifests/params.pp
@@ -13,12 +13,14 @@ class puppet::params {
             $server_packages = [ 'puppet-server', 'puppet-tools', ]
 
             # Apparently the brilliant packagers at puppetlabs have no clue
-            # what stability means or why it's a good thing.
-            if versioncmp($puppetversion, '3.1') < 0 or
-               versioncmp($puppetversion, '3.4.0') >= 0 {
-                $client_service_name = 'puppet'
-            } else {
+            # what stability means or why it's a good thing.  See:
+            # https://tickets.puppetlabs.com/browse/PUP-1200
+            if $::operatingsystemrelease >= 19 and
+               versioncmp($puppetversion, '3.1.1') >= 0 and
+               versioncmp($puppetversion, '3.4.0') < 0 {
                 $client_service_name = 'puppetagent'
+            } else {
+                $client_service_name = 'puppet'
             }
 
             $server_service_name = 'puppetmaster'
