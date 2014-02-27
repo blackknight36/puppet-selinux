76321acbeac25c6e1ea216e928fa00091eb6ba54
Chris Kennedy <chris_kennedy@dart.biz>
2014-02-27 08:45:32 -0500

    Prepare rsync config for all PICAPS servers.  Add mdct-98pi.

---
+++ modules/dart/manifests/mdct_98pi.pp
@@ -0,0 +1,24 @@
+# modules/dart/manifests/mdct_98pi.pp
+#
+# Synopsis:
+#       PICAPS TestPlant98 server
+#
+# Contact:
+#       Chris Kennedy
+
+class dart::mdct_98pi inherits dart::abstract::picaps_production_server_node {
+
+    class { 'network':
+        service         => 'legacy',
+        domain          => 'dartcontainer.com',
+        name_servers    => ['10.1.0.98', '10.1.0.99'],
+    }
+
+    network::interface { 'em1':
+        template    => 'static',
+        ip_address  => '10.1.192.149',
+        netmask     => '255.255.0.0',
+        gateway     => '10.1.0.25',
+    }
+
+}
