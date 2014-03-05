08d9a11473671b201c9d1bf37a84d1db7c874a06
Chris Kennedy <chris_kennedy@dart.biz>
2014-03-05 09:44:43 -0500

    New puppet nodes for upcoming PICAPS server upgrades: Mason, Aurora

---
+++ modules/dart/manifests/mdct_04pi.pp
@@ -0,0 +1,24 @@
+# modules/dart/manifests/mdct_04pi.pp
+#
+# Synopsis:
+#       PICAPS live server for Aurora
+#
+# Contact:
+#       Chris Kennedy
+
+class dart::mdct_04pi inherits dart::abstract::picaps_production_server_node {
+
+    class { 'network':
+        service         => 'legacy',
+        domain          => 'dartcontainer.com',
+        name_servers    => ['10.1.0.98', '10.1.0.99'],
+    }
+
+#    network::interface { 'em1':
+#        template    => 'static',
+#        ip_address  => '10.1.192.149',
+#        netmask     => '255.255.0.0',
+#        gateway     => '10.1.0.25',
+#    }
+
+}
