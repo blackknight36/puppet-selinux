0dcafe0ded3667b2a4306f6436a322c8bccdd73f
Chris Kennedy <chris_kennedy@dart.biz>
2014-03-18 19:02:21 -0400

    Added class for Corona PICAPS server

---
+++ modules/dart/manifests/mdct_05pi.pp
@@ -0,0 +1,24 @@
+# modules/dart/manifests/mdct_05pi.pp
+#
+# Synopsis:
+#       PICAPS live server for Corona
+#
+# Contact:
+#       Chris Kennedy
+
+class dart::mdct_05pi inherits dart::abstract::picaps_production_server_node {
+
+    class { 'network':
+        service         => 'legacy',
+        domain          => 'dartcontainer.com',
+        name_servers    => ['10.1.0.98', '10.1.0.99'],
+    }
+
+#    network::interface { 'em1':
+#        template    => 'static',
+#        ip_address  => '10.1.192.149',
+#        netmask     => '255.255.0.0',
+#        gateway     => '10.1.0.25',
+#    }
+
+}
