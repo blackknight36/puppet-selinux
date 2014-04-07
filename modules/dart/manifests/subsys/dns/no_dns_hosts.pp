949ebbf255c1dc6795524646bb1b35360367b11a
John Florian <john_florian@dart.biz>
2014-04-07 13:41:54 -0400

    Drop - obsolete no_dns_hosts
    These have been "ensure => absent" for sufficiently long enough that
    most all nodes should have applied already.

--- modules/dart/manifests/subsys/dns/no_dns_hosts.pp
+++ modules/dart/manifests/subsys/dns/no_dns_hosts.pp
@@ -30,36 +30,6 @@ class dart::subsys::dns::no_dns_hosts {
         host_aliases    => [ 'mdct-dev19' ],
     }
 
-    host { 'mdct-f8-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.132',
-        host_aliases    => [ 'mdct-f8-builder', 'f8b' ],
-    }
-
-    host { 'mdct-f10-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.133',
-        host_aliases    => [ 'mdct-f10-builder', 'f10b' ],
-    }
-
-    host { 'mdct-f11-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.135',
-        host_aliases    => [ 'mdct-f11-builder', 'f11b' ],
-    }
-
-    host { 'mdct-f12-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.134',
-        host_aliases    => [ 'mdct-f12-builder', 'f12b' ],
-    }
-
-    host { 'mdct-f13-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.136',
-        host_aliases    => [ 'mdct-f13-builder', 'f13b' ],
-    }
-
     host { 'mdct-f14-builder.dartcontainer.com':
         ip              => '10.1.192.137',
         host_aliases    => [ 'mdct-f14-builder', 'f14b' ],
@@ -78,24 +48,6 @@ class dart::subsys::dns::no_dns_hosts {
     host { 'mdct-f17-builder.dartcontainer.com':
         ip              => '10.1.192.143',
         host_aliases    => [ 'mdct-f17-builder', 'f17b' ],
-    }
-
-    host { 'mdct-f18-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.145',
-        host_aliases    => [ 'mdct-f18-builder', 'f18b' ],
-    }
-
-    host { 'mdct-f19-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.155',
-        host_aliases    => [ 'mdct-f19-builder', 'f19b' ],
-    }
-
-    host { 'mdct-f20-builder.dartcontainer.com':
-        ensure          => absent,
-        ip              => '10.1.192.132',
-        host_aliases    => [ 'mdct-f20-builder', 'f20b' ],
     }
 
     host { 'mdct-koji.dartcontainer.com':
