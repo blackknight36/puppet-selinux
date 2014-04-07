7be7c6591c57e78c808aee49781aafc978bb3311
John Florian <john_florian@dart.biz>
2014-04-07 13:43:14 -0400

    New - host node mdct_aos_master_f20
    This will be superseding mdct_aos_master_f19 shortly.  It is being
    implemented to facilitate migration to Python3.  (python3-django is not
    available on Fedora 19.)

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -35,6 +35,7 @@ node 'mdct-76pt.dartcontainer.com'              { include 'dart::mdct_76pt' }
 node 'mdct-77pt.dartcontainer.com'              { include 'dart::mdct_77pt' }
 node 'mdct-99pi.dartcontainer.com'              { include 'dart::mdct_99pi' }
 node 'mdct-aos-master-f19.dartcontainer.com'    { include 'dart::mdct_aos_master_f19' }
+node 'mdct-aos-master-f20.dartcontainer.com'    { include 'dart::mdct_aos_master_f20' }
 node 'mdct-ci-agent1.dartcontainer.com'         { include 'dart::mdct_ci_agent1' }
 node 'mdct-dev10.dartcontainer.com'             { include 'dart::mdct_dev10' }
 node 'mdct-dev11.dartcontainer.com'             { include 'dart::mdct_dev11' }
---
+++ modules/dart/manifests/mdct_aos_master_f20.pp
@@ -0,0 +1,29 @@
+# modules/dart/manifests/mdct_aos_master_f20.pp
+#
+# == Class: dart::mdct_aos_master_f20
+#
+# Configures the AOS Master host.
+#
+# === Parameters
+#
+# NONE
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+class dart::mdct_aos_master_f20 {
+
+    class { 'bacula::client':
+        dir_passwd  => 'hobe9yveB940mWreqoVzfTMIXfWurWi5ROughoJw7A39',
+        mon_passwd  => 'zdJxxdFhes9YxlJNhAHFOaSbneIy9N3FmlzTkA1wdowU',
+    }
+
+    class { 'dart::abstract::aos_master_node':
+        django_user     => 'django',
+        django_group    => 'django',
+        python_ver      => '2.7',
+    }
+
+}
