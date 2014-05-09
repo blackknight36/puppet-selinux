d94348ef5e911a0dc531e78876412d9e890a331c
Chris Kennedy <chris_kennedy@dart.biz>
2014-05-09 12:15:01 -0400

    Added PICAPS production server mdct-73pi

---
+++ modules/dart/manifests/mdct_73pi.pp
@@ -0,0 +1,24 @@
+# modules/dart/manifests/mdct_73pi.pp
+#
+# Synopsis:
+#       PICAPS live server for Argentina
+#
+# Contact:
+#       Chris Kennedy
+
+class dart::mdct_73pi inherits dart::abstract::picaps_production_server_node {
+
+    class { 'network':
+        service         => 'legacy',
+        domain          => 'dartcontainer.com',
+        name_servers    => ['10.1.0.98', '10.1.0.99'],
+    }
+
+    #network::interface { 'em1':
+    #    template    => 'static',
+    #    ip_address  => '10.1.192.151',
+    #    netmask     => '255.255.0.0',
+    #    gateway     => '10.1.0.25',
+    #}
+
+}
