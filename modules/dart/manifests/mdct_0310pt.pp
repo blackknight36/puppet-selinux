96557e3d5d0f7bad824f6dde7da9ef0c2ef12387
Nate Nephew <nathan.nephew@dart.biz>
2014-05-29 14:41:56 -0400

    add Chicago picaps test server

---
+++ modules/dart/manifests/mdct_0310pt.pp
@@ -0,0 +1,11 @@
+# modules/dart/manifests/mdct_0310pt.pp
+#
+# Synopsis:
+#       PICAPS test server for Chicago
+#
+# Contact:
+#       Nathan Nephew
+
+class dart::mdct_0310pt inherits dart::abstract::picaps_test_server_node {
+
+}
