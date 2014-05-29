539893ffcc5ab2b635476adec1024ab543d9bbe4
Nate Nephew <nathan.nephew@dart.biz>
2014-05-29 14:39:06 -0400

    start managing tcp ports

--- modules/dart/manifests/mdct_dev9.pp
+++ modules/dart/manifests/mdct_dev9.pp
@@ -10,5 +10,9 @@ class dart::mdct_dev9 inherits dart::abstract::workstation_node {
 
     include 'jetbrains::idea'
     include 'dart::subsys::yum_cron'
+    iptables::tcp_port {
+        'picapswebui-local': port => '8180';
+        'synergy': port => '24800';
+    }
 
 }
