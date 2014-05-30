63eec14458f1f6827d7a060bbc2e4f7e52a8cb55
John Florian <john_florian@dart.biz>
2014-05-30 08:18:18 -0400

    New - iscsi module
    This is presently very limited in scope, but does install the iSCSI
    initiator tools on a host.  It offers nothing to configure targets
    (using iscsiadm) -- that's a one-time task that may be best left to an
    administrator.

---
+++ modules/iscsi/manifests/params.pp
@@ -0,0 +1,32 @@
+# modules/iscsi/manifests/params.pp
+#
+# == Class: iscsi::params
+#
+# Parameters for the iscsi puppet module.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+class iscsi::params {
+
+    case $::operatingsystem {
+        Fedora: {
+
+            $packages = [
+                'iscsi-initiator-utils',
+            ]
+            $service_name = [
+                'iscsi.service',
+            ]
+
+        }
+
+        default: {
+            fail ("The iscsi module is not yet supported on ${::operatingsystem}.")
+        }
+
+    }
+
+}
