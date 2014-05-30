46fd7ef79231b878066c1ce1684f8c8183b6e58e
John Florian <john_florian@dart.biz>
2014-05-30 08:23:11 -0400

    New - manage iSCSI on mdct-00bk
    This is limited to only ensuring the appropriate tools are in place.
    The administrator must still connect the desired target(s).

--- modules/dart/manifests/mdct_00bk.pp
+++ modules/dart/manifests/mdct_00bk.pp
@@ -8,18 +8,19 @@
 
 class dart::mdct_00bk inherits dart::abstract::guarded_server_node {
 
+    include 'iscsi::initiator'
+
     class { 'puppet::client':
+    }
+
+    class { 'postgresql::server':
+        hba_conf    => 'puppet:///private-host/postgresql/pg_hba.conf',
     }
 
     class { 'bacula::console':
         dir_address => $dart::params::bacula_dir_fqdn,
         dir_name    => $dart::params::bacula_dir_name,
         dir_passwd  => $dart::params::bacula_dir_passwd,
-    }
-
-    class { 'bacula::director':
-        dir_conf        => template('dart/bacula/director.conf'),
-        pgpass_source   => 'puppet:///private-host/.pgpass',
     }
 
     class { 'bacula::storage_daemon':
@@ -31,8 +32,10 @@ class dart::mdct_00bk inherits dart::abstract::guarded_server_node {
         sd_archive_dev  => '/storage/backup',
     }
 
-    class { 'postgresql::server':
-        hba_conf    => 'puppet:///private-host/postgresql/pg_hba.conf',
+    class { 'bacula::director':
+        dir_conf        => template('dart/bacula/director.conf'),
+        pgpass_source   => 'puppet:///private-host/.pgpass',
+        require         => Class['postgresql::server'],
     }
 
     bacula::director::inclusion {
