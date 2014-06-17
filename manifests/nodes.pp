ab50a01d0fc5d48664e4501dd699adfb16ee419a
Chris Kennedy <chris_kennedy@dart.biz>
2014-06-17 10:13:28 -0400

    Added PICAPS server node for mdct-77pi and mdct-99pi-x

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -46,8 +46,10 @@ node 'mdct-75pi.dartcontainer.com'              { include 'dart::mdct_75pi' }
 node 'mdct-75pt.dartcontainer.com'              { include 'dart::mdct_75pt' }
 node 'mdct-76pi.dartcontainer.com'              { include 'dart::mdct_76pi' }
 node 'mdct-76pt.dartcontainer.com'              { include 'dart::mdct_76pt' }
+node 'mdct-77pi.dartcontainer.com'              { include 'dart::mdct_77pi' }
 node 'mdct-77pt.dartcontainer.com'              { include 'dart::mdct_77pt' }
 node 'mdct-99pi.dartcontainer.com'              { include 'dart::mdct_99pi' }
+node 'mdct-99pi-x.dartcontainer.com'            { include 'dart::mdct_99pi_x' }
 node 'mdct-aos-master-f19.dartcontainer.com'    { include 'dart::mdct_aos_master_f19' }
 node 'mdct-aos-master-f20.dartcontainer.com'    { include 'dart::mdct_aos_master_f20' }
 node 'mdct-ci-agent1.dartcontainer.com'         { include 'dart::mdct_ci_agent1' }
---
+++ modules/dart/manifests/mdct_77pi.pp
@@ -0,0 +1,24 @@
+# modules/dart/manifests/mdct_77pi.pp
+#
+# Synopsis:
+#       PICAPS live server for Canada
+#
+# Contact:
+#       Chris Kennedy
+
+class dart::mdct_77pi inherits dart::abstract::picaps_production_server_node {
+
+    class { 'network':
+        service         => 'legacy',
+        domain          => $dart::params::dns_domain,
+        name_servers    => $dart::params::dns_servers,
+    }
+
+#    network::interface { 'em1':
+#        template    => 'static',
+#        ip_address  => '10.1.192.149',
+#        netmask     => '255.255.0.0',
+#        gateway     => '10.1.0.25',
+#    }
+
+}
