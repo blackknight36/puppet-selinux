f8854cf316d026fe94d3ae493acfc40c4a130120
Chris Kennedy <chris_kennedy@dart.biz>
2014-07-01 12:01:59 -0400

    Introduced node for Lithonia production PICAPS server mdct-03pi

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -25,6 +25,7 @@ node 'mdct-02pi.dartcontainer.com'              { include 'dart::mdct_02pi' }
 node 'mdct-0302pi.dartcontainer.com'            { include 'dart::mdct_0302pi' }
 node 'mdct-0310pi.dartcontainer.com'            { include 'dart::mdct_0310pi' }
 node 'mdct-0314pi.dartcontainer.com'            { include 'dart::mdct_0314pi' }
+node 'mdct-03pi.dartcontainer.com'              { include 'dart::mdct_03pi' }
 node 'mdct-03pt.dartcontainer.com'              { include 'dart::mdct_03pt' }
 node 'mdct-04pi.dartcontainer.com'              { include 'dart::mdct_04pi' }
 node 'mdct-04pt.dartcontainer.com'              { include 'dart::mdct_04pt' }
