3f2fc0241e7269ca555cf7ea0a68c9598accdf56
Levi Harper <levi.harper@dart.biz>
2014-07-15 09:52:29 -0400

    Add mdct-dev21 workstation for Caspian.

---
+++ modules/dart/manifests/mdct_dev21.pp
@@ -0,0 +1,18 @@
+# modules/dart/manifests/mdct_dev21.pp
+#
+# Synopsis:
+#       Developer Workstation
+#
+# Contact:
+#       Caspian Peavyhouse
+
+class dart::mdct_dev21 inherits dart::abstract::workstation_node {
+
+    include 'dart::subsys::yum_cron'
+    include 'jetbrains::idea'
+
+    iptables::tcp_port {
+        'tomcat': port => '8080';
+    }
+
+}
