7484243052538ce0f5556fbd450e8a5b2ca62fd5
John Florian <john_florian@dart.biz>
2014-07-17 16:56:30 -0400

    New - "xdg_desktop" module
    This module provides an interface to the XDG tools suite to conform to
    the XDG Desktop Menu Specifications.

---
+++ modules/xdg_desktop/manifests/menu_entry.pp
@@ -0,0 +1,58 @@
+# modules/xdg_desktop/manifests/menu_entry.pp
+#
+# == Define: xdg_desktop::menu_entry
+#
+# Installs a desktop menu entry per the XDG Desktop Menu Specification.
+#
+# === Parameters
+#
+# [*namevar*]
+#   An arbitrary identifier for the menu entry instance.
+#
+# [*ensure*]
+#   Instance is to be 'present' (default) or 'absent'.
+#
+# [*dir_file*]
+#   Path to the "directory file", which must already be deployed.  See
+#   xdg-desktop-menu(1) for details.
+#
+# [*desktop_file*]
+#   Path to the "desktop file", which must already be deployed.  See
+#   xdg-desktop-menu(1) for details.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+define xdg_desktop::menu_entry (
+        $ensure='present',
+        $dir_file,
+        $desktop_file,
+    ) {
+
+    include 'xdg_desktop::params'
+
+    case $ensure {
+
+        'present': {
+            exec { "install-xdg-menu-entry-${name}":
+                command => "${xdg_desktop::params::menu_command} install ${dir_file} ${desktop_file}",
+                creates => "${xdg_desktop::params::app_dir}/${name}.desktop",
+            }
+        }
+
+        'absent': {
+            exec { "uninstall-xdg-menu-entry-${name}":
+                command => "${xdg_desktop::params::menu_command} install ${dir_file} ${desktop_file}",
+                onlyif  => "test -e \"${xdg_desktop::params::app_dir}/${name}.desktop\"",
+            }
+        }
+
+        default: {
+            fail('$ensure must be either "present" or "absent"')
+        }
+
+    }
+
+}
---
+++ modules/xdg_desktop/manifests/params.pp
@@ -0,0 +1,28 @@
+# modules/xdg_desktop/manifests/params.pp
+#
+# == Class: xdg_desktop::params
+#
+# Parameters for the xdg_desktop puppet module.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+class xdg_desktop::params {
+
+    case $::operatingsystem {
+        Fedora: {
+
+            $menu_command = 'xdg-desktop-menu'
+            $app_dir = '/usr/local/share/applications'
+
+        }
+
+        default: {
+            fail ("The xdg_desktop module is not yet supported on ${::operatingsystem}.")
+        }
+
+    }
+
+}
