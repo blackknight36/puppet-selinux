6064c704875881907c780fa5b8a22f9c4daa7d49
John Florian <john_florian@dart.biz>
2014-07-25 11:53:25 -0400

    New - PyCharm (CE) on Caspian's workstation

--- modules/dart/manifests/mdct_dev21.pp
+++ modules/dart/manifests/mdct_dev21.pp
@@ -8,6 +8,7 @@
 
 class dart::mdct_dev21 inherits dart::abstract::workstation_node {
 
+    include 'dart::abstract::pycharm::community'
     include 'dart::subsys::yum_cron'
     include 'jetbrains::idea'
 
