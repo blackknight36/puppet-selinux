082683d2c5a0718b71ace057bba420dbc46062cb
John Florian <john_florian@dart.biz>
2014-08-05 09:10:32 -0400

    Fix - plant_number fact lacks coverage for Solo
    This fact has been freshened using the latest of the AOS puppet
    resources.  They're essentially the same now, except for the case where
    no default route exists: the AOS fact returns "unknown" then whereas
    this fact continues to return nil for backwards compatibility.

--- modules/dart/lib/facter/plant_number.rb
+++ modules/dart/lib/facter/plant_number.rb
@@ -1,30 +1,49 @@
-# modules/dart/files/facter/plant_number.rb
+# modules/dart/lib/facter/plant_number.rb
 #
 # Synopsis:
-#       Determine the plant number by parsing out the 2nd octet of the host's
-#       default route.  The value returned will be a two digit code,
-#       left-padded with zeroes, e.g., (Mason ==> "01").  If no default route
-#       exists, nil will be returned.
+#       Determine the plant number by mapping known default routes for each
+#       plant entity.  The value returned will be a two digit code,
+#       left-padded with zeroes, e.g., (Mason ==> "01") unless two digits is
+#       insufficient in which case the value returned will be a four digit
+#       code, e.g., (Dallas ==> "0314").  If no default route exists, nil will
+#       be returned.
+#
+# Author: John Florian <john_florian@dart.biz>
+# Copyright 2011-2014 Dart Container Corp.
+
 
 Facter.add("plant_number") do
     begin
         output = %x{/sbin/ip route show 0.0.0.0/0}
         gw = output.split()[2]
-        if gw
-            subnet = "%02d" % gw.split(".")[1]
-        else
-            subnet = nil
-        end
+        gw_to_plant_map = {
+            '10.1.0.25'     => '01',
+            '10.2.0.25'     => '02',
+            '10.3.0.25'     => '03',
+            '10.6.0.25'     => '04',
+            '10.5.0.25'     => '05',
+            '10.10.0.25'    => '10',
+            '10.11.0.25'    => '11',
+            '10.15.0.25'    => '15',
+            '10.19.0.25'    => '19',
+            '10.25.0.25'    => '25',
+            '10.39.0.25'    => '39',
+            '10.47.0.25'    => '47',
+            '10.52.0.25'    => '52',
+            '10.55.0.25'    => '55',
+            '10.72.0.25'    => '72',
+            '10.73.0.25'    => '73',
+            '10.75.0.25'    => '75',
+            '10.76.0.25'    => '76',
+            '10.77.0.25'    => '77',
+            '10.78.0.25'    => '78',
+            '10.96.0.25'    => '96',
+            '10.7.87.254'   => '0302',
+            '10.31.55.254'  => '0310',
+            '10.7.215.254'  => '0314',
+        }
     end
     setcode do
-        case subnet
-        when '06'
-            # Aurora, IL is an exception.  Originally subnetted as 10.4/16,
-            # they've migrated to 10.6/16 to accommodate integration of the
-            # Solo network, yet remain plant# 04.
-            '04'
-        else
-            subnet
-        end
+        gw_to_plant_map[gw] or nil
     end
 end
