c162f19d42bbc2aaddb8db004c029cd5c2f31ff4
John Florian <john_florian@dart.biz>
2014-08-11 13:43:53 -0400

    New - "lyx" module

---
+++ modules/lyx/manifests/params.pp
@@ -0,0 +1,28 @@
+# modules/lyx/manifests/params.pp
+#
+# == Class: lyx::params
+#
+# Parameters for the lyx puppet module.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+class lyx::params {
+
+    case $::operatingsystem {
+        Fedora: {
+
+            $packages = [
+                'lyx',
+            ]
+        }
+
+        default: {
+            fail ("The lyx module is not yet supported on ${::operatingsystem}.")
+        }
+
+    }
+
+}
