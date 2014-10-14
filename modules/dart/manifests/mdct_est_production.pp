f47a83c760e4c1d424d1e3e12a40c07f312343a0
Collin Baker <collin.baker@dart.biz>
2014-10-14 11:43:50 -0400

    Created new EST production node.

---
+++ modules/dart/manifests/mdct_est_production.pp
@@ -0,0 +1,14 @@
+# modules/dart/manifests/mdct_00dw.pp
+#
+# Synopsis:
+#       EST data warehouse
+#
+# Contact:
+#       Ben Minshall
+
+class dart::mdct_00dw inherits dart::abstract::guarded_server_node {
+
+    class { 'puppet::client':
+    }
+
+}
