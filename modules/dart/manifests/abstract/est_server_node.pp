cc246902d3594736117acef1b7e1413c47a4cbda
Collin Baker <collin.baker@dart.biz>
2014-10-14 16:37:10 -0400

    Removed Iptables class from est server node

--- modules/dart/manifests/abstract/est_server_node.pp
+++ modules/dart/manifests/abstract/est_server_node.pp
@@ -1,9 +1,6 @@
 # modules/dart/manifests/abstract/est_server_node.pp
 
 class dart::abstract::est_server_node inherits dart::abstract::tomcat_web_app {
-    class { 'iptables':
-        enabled => true,
-    }
 
     package { [ 'postgresql.x86_64', 'postgresql-contrib.x86_64', 'postgresql-jdbc.noarch', 'postgresql-libs.x86_64', 'postgresql-server.x86_64', ]:
         ensure  => installed,
