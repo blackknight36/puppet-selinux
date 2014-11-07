09ecb6def03ee8af60645046a08e99d10064c688
John Florian <john_florian@dart.biz>
2014-11-07 09:47:27 -0500

    Fix - systemd::unit not restarted after content change
    If a unit file's content is changed the unit should be restarted
    immediately.  This does seem scary for a service where high-availability
    is expected, but is arguably better than having that services behavior
    change at some indeterminate future time where it becomes increasingly
    difficult to trace the behavioral change back to the cause that effected
    it.

--- modules/systemd/manifests/unit.pp
+++ modules/systemd/manifests/unit.pp
@@ -109,11 +109,12 @@ define systemd::unit (
             unless  => "systemctl is-active ${target}",
         }
 
-        if $restart_events != undef {
-            exec { "systemctl restart ${target}":
-                refreshonly => true,
-                subscribe   => $restart_events,
-            }
+        exec { "systemctl restart ${target}":
+            refreshonly => true,
+            subscribe   => [
+                File[$fqfn],
+                $restart_events,
+            ],
         }
 
     } elsif $ensure == 'absent' {
