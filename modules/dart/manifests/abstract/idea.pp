48ae24e4cb54e00cfd45316fb048540a977ad4f3
Levi Harper <levi.harper@dart.biz>
2014-12-16 11:43:49 -0500

    Add IDEA 14.0.2.

--- modules/dart/manifests/abstract/idea.pp
+++ modules/dart/manifests/abstract/idea.pp
@@ -22,6 +22,10 @@ class dart::abstract::idea {
     # to ensure that no more than two stable releases are installed at any
     # given time.
 
+    jetbrains::idea::release { 'ideaIU-14.0.2':
+        build   => '139.659.2',
+    }
+
     jetbrains::idea::release { 'ideaIU-14':
         build   => '139.224.1',
     }
