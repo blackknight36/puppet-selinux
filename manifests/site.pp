214e0744a98e63419068dbcd70b4929027f9634b
John Florian <john_florian@dart.biz>
2015-01-05 14:07:03 -0500

    Janitorial - puppet-lint compliance

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 # manifests/site.pp
 
-import "nodes"
+import 'nodes'
 
 # The filebucket option allows for file backups to the server
-filebucket { "main":
-    server  => "puppet.dartcontainer.com",
+filebucket { 'main':
+    server  => 'puppet.dartcontainer.com',
 }
 
 # Establish run stages as: FIRST precedes EARLY precedes MAIN precedes FINAL
@@ -49,13 +49,13 @@ Exec {
     # Ignore output of any exec, unless there was a failure.
     logoutput   => 'on_failure',
     # Match Fedora's default PATH.
-    path        => "/usr/bin:/usr/sbin/:/bin:/sbin",
+    path        => '/usr/bin:/usr/sbin/:/bin:/sbin',
 }
 
 File {
     # For any overwritten files, keep backups in the main filebucket (defined
     # above).
-    backup  => "main",
+    backup  => 'main',
 }
 
 Package {
