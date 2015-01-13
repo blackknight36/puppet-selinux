e45c6fe1167f851a93260fc6c2acd7425603fe35
John Florian <john_florian@dart.biz>
2015-01-13 10:46:28 -0500

    Refactor - simplify PICAPS Test host classes
    There's absolutely no need for this class proliferation when all 20 host
    classes are identical.  If one does need to be different, that would be
    the time to implement such a class.  Otherwise, it's a much simpler
    arrangement to have all PICAPS Test hosts be directly associated with
    the abstract class.
    
    This also replaces the 20 hostname-to-class associations with a single
    association based on a strict regular-expression.
    
    Lots of reduction, same great taste!

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -20,47 +20,28 @@ node 'mdct-00sw.dartcontainer.com'              { include 'dart::mdct_00sw' }
 node 'mdct-00tl.dartcontainer.com'              { include 'dart::mdct_00tl' }
 node 'mdct-00pi.dartcontainer.com'              { include 'dart::mdct_00pi' }
 node 'mdct-01pi.dartcontainer.com'              { include 'dart::mdct_01pi' }
-node 'mdct-01pt.dartcontainer.com'              { include 'dart::mdct_01pt' }
 node 'mdct-02pi.dartcontainer.com'              { include 'dart::mdct_02pi' }
 node 'mdct-0302pi.dartcontainer.com'            { include 'dart::mdct_0302pi' }
-node 'mdct-0302pt.dartcontainer.com'            { include 'dart::mdct_0302pt' }
 node 'mdct-0310pi.dartcontainer.com'            { include 'dart::mdct_0310pi' }
-node 'mdct-0310pt.dartcontainer.com'            { include 'dart::mdct_0310pt' }
 node 'mdct-0314pi.dartcontainer.com'            { include 'dart::mdct_0314pi' }
 node 'mdct-03pi.dartcontainer.com'              { include 'dart::mdct_03pi' }
-node 'mdct-03pt.dartcontainer.com'              { include 'dart::mdct_03pt' }
 node 'mdct-04pi.dartcontainer.com'              { include 'dart::mdct_04pi' }
-node 'mdct-04pt.dartcontainer.com'              { include 'dart::mdct_04pt' }
 node 'mdct-05pi.dartcontainer.com'              { include 'dart::mdct_05pi' }
-node 'mdct-05pt.dartcontainer.com'              { include 'dart::mdct_05pt' }
 node 'mdct-10pi.dartcontainer.com'              { include 'dart::mdct_10pi' }
-node 'mdct-10pt.dartcontainer.com'              { include 'dart::mdct_10pt' }
 node 'mdct-11pi.dartcontainer.com'              { include 'dart::mdct_11pi' }
 node 'mdct-15pi.dartcontainer.com'              { include 'dart::mdct_15pi' }
-node 'mdct-15pt.dartcontainer.com'              { include 'dart::mdct_15pt' }
 node 'mdct-19pi.dartcontainer.com'              { include 'dart::mdct_19pi' }
-node 'mdct-19pt.dartcontainer.com'              { include 'dart::mdct_19pt' }
 node 'mdct-25pi.dartcontainer.com'              { include 'dart::mdct_25pi' }
-node 'mdct-25pt.dartcontainer.com'              { include 'dart::mdct_25pt' }
 node 'mdct-39pi.dartcontainer.com'              { include 'dart::mdct_39pi' }
-node 'mdct-39pt.dartcontainer.com'              { include 'dart::mdct_39pt' }
 node 'mdct-47pi.dartcontainer.com'              { include 'dart::mdct_47pi' }
-node 'mdct-47pt.dartcontainer.com'              { include 'dart::mdct_47pt' }
 node 'mdct-52pi.dartcontainer.com'              { include 'dart::mdct_52pi' }
 node 'mdct-55pi.dartcontainer.com'              { include 'dart::mdct_55pi' }
-node 'mdct-55pt.dartcontainer.com'              { include 'dart::mdct_55pt' }
 node 'mdct-72pi.dartcontainer.com'              { include 'dart::mdct_72pi' }
-node 'mdct-72pt.dartcontainer.com'              { include 'dart::mdct_72pt' }
 node 'mdct-73pi.dartcontainer.com'              { include 'dart::mdct_73pi' }
-node 'mdct-73pt.dartcontainer.com'              { include 'dart::mdct_73pt' }
 node 'mdct-75pi.dartcontainer.com'              { include 'dart::mdct_75pi' }
-node 'mdct-75pt.dartcontainer.com'              { include 'dart::mdct_75pt' }
 node 'mdct-76pi.dartcontainer.com'              { include 'dart::mdct_76pi' }
-node 'mdct-76pt.dartcontainer.com'              { include 'dart::mdct_76pt' }
 node 'mdct-77pi.dartcontainer.com'              { include 'dart::mdct_77pi' }
-node 'mdct-77pt.dartcontainer.com'              { include 'dart::mdct_77pt' }
 node 'mdct-78pi.dartcontainer.com'              { include 'dart::mdct_78pi' }
-node 'mdct-78pt.dartcontainer.com'              { include 'dart::mdct_78pt' }
 node 'mdct-99pi.dartcontainer.com'              { include 'dart::mdct_99pi' }
 node 'mdct-99pi-x.dartcontainer.com'            { include 'dart::mdct_99pi_x' }
 node 'mdct-aos-master-f19.dartcontainer.com'    { include 'dart::mdct_aos_master_f19' }
@@ -110,6 +91,7 @@ node 'tc-util.dartcontainer.com'                { include 'dart::tc_util' }
 #
 # This section consists only of loose host(s):class relations on a M:1 basis.
 #
+node /^mdct-[0-9]{2,4}pt.dartcontainer.com$/    { include 'dart::abstract::picaps_test_server_node' }
 node /^mdct-dev10-.*\.dartcontainer\.com$/      { include 'dart::mdct_dev10_srvr' }
 node /^mdct-ovirt-node-.*\.dartcontainer\.com$/ { include 'dart::mdct_ovirt_node' }
 node /^mdct-puppet.*\.dartcontainer\.com$/      { include 'dart::mdct_puppet' }
