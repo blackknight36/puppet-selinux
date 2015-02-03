fd0720f81bfbe50dbf1b6998d6b648bf2f6d155b
Chris Kennedy <chris_kennedy@dart.biz>
2015-02-03 14:45:57 -0500

    Upgrade mdct-47pi to Fedora 20

--- modules/dart/manifests/mdct_47pi.pp
+++ modules/dart/manifests/mdct_47pi.pp
@@ -6,19 +6,19 @@
 # Contact:
 #       Chris Kennedy
 
-class dart::mdct-1047pi inherits dart::abstract::picaps_production_server_node {
+class dart::mdct_47pi inherits dart::abstract::picaps_production_server_node {
 
-    class { 'network':
-        service         => 'legacy',
-        domain          => $dart::params::dns_domain,
-        name_servers    => $dart::params::dns_servers,
-    }
+    #class { 'network':
+    #    service         => 'legacy',
+    #    domain          => $dart::params::dns_domain,
+    #    name_servers    => $dart::params::dns_servers,
+    #}
 
-    network::interface { 'em1':
-        template    => 'static',
-        ip_address  => '10.1.192.149',
-        netmask     => '255.255.0.0',
-        gateway     => '10.1.0.25',
-    }
+    #network::interface { 'em1':
+    #    template    => 'static',
+    #    ip_address  => '10.1.192.149',
+    #    netmask     => '255.255.0.0',
+    #    gateway     => '10.1.0.25',
+    #}
 
 }
