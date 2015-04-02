212504c2b302ea6bc6dabab1e6d5ef69c41f4106
John Florian <john_florian@dart.biz>
2015-04-02 08:39:02 -0400

    New - improve docs for our Koji parameters
    This commit simply adds a bit of commentary for the global parameters of
    our Koji deployment and groups them by function.

--- modules/dart/manifests/subsys/koji/params.pp
+++ modules/dart/manifests/subsys/koji/params.pp
@@ -11,19 +11,28 @@
 
 class dart::subsys::koji::params {
 
-    # An alternate home directory for local users so as to not clash with
-    # autofs which dominates /home.
-    $local_homes = '/var/local/home'
-
+    # host providing the Koji Hub, an XML-RPC server that coordinates all
+    # resources and tasks
     $hub_host   = 'mdct-koji-f21.dartcontainer.com'
 
-    $db_passwd  = 'mdct.koji'
-    $db_user    = 'koji'
+    # well known URLs on the Koji Hub
     $downloads  = "http://${hub_host}/kojifiles"
     $hub        = "http://${hub_host}/kojihub"
+
+    # an alternate home directory for local users so as to not clash with
+    # autofs which dominates /home
+    $local_homes = '/var/local/home'
+
+    # details regarding the Koji database
+    $db_passwd  = 'mdct.koji'
+    $db_user    = 'koji'
+
+    # various directory locations used by one or more Koji services
     $repodir    = '/mnt/mdct-new-repo'
     $topdir     = '/mnt/koji'
     $workdir    = '/var/tmp/koji'
+
+    # details regard the Koji Web service
     $web_passwd = 'D0gG0n31t'
 
 }
