b846ddd88c5e27031b5e69d0f5382ca9cdaefe7a
John Florian <john_florian@dart.biz>
2015-04-07 11:21:40 -0400

    New - Koji builder #2 (mdct-koji-b2-f21)
    This adds a 2nd builder to handle tasks for the i386 and x86_64 hardware
    architectures.

--- modules/dart/manifests/subsys/dns/no_dns_hosts.pp
+++ modules/dart/manifests/subsys/dns/no_dns_hosts.pp
@@ -55,6 +55,11 @@ class dart::subsys::dns::no_dns_hosts {
         host_aliases    => [ 'mdct-koji-b1-f21', 'mdct-koji-b1' ],
     }
 
+    host { 'mdct-koji-b2-f21.dartcontainer.com':
+        ip              => '10.1.192.136',
+        host_aliases    => [ 'mdct-koji-b2-f21', 'mdct-koji-b2' ],
+    }
+
     host { 'mdct-koji-f21.dartcontainer.com':
         ip              => '10.1.192.124',
         host_aliases    => [ 'mdct-koji-f21', 'mdct-koji', 'koji' ],
