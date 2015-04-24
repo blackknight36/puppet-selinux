593df28d66839ce51af910bec631b2829303ea10
John Florian <john_florian@dart.biz>
2015-04-24 16:14:03 -0400

    Janitorial - use absolute namespace references

--- modules/dart/manifests/mdct_dev12.pp
+++ modules/dart/manifests/mdct_dev12.pp
@@ -18,32 +18,33 @@ class dart::mdct_dev12 inherits dart::abstract::workstation_node {
     # Apache httpd is needed for at least serving man2html, but may be used
     # for development and/or debugging setups of dhcpd-driven-master or
     # firewall-driven-master, hence the network_connect setting.
-    class { 'apache':
+    class { '::apache':
         network_connect => true,
     }
 
-    class { 'bacula::admin':
+    class { '::bacula::admin':
         dir_address => "mdct-00bk.${::domain}",
         dir_name    => 'mdct-00bk-dir',
         dir_passwd  => 'a/kIuMrD+AIJxl5HlDZhdEdugagOer5nUi43qgip2DED',
     }
 
-    class { 'bacula::client':
-        dir_name   => $dart::params::bacula_dir_name,
+    class { '::bacula::client':
+        dir_name   => $::dart::params::bacula_dir_name,
         dir_passwd => 'RB9c3KBgpRyXzbGC6wXCEA5ao2SAtgAyt24tD8pni17h',
-        mon_name   => $dart::params::bacula_mon_name,
+        mon_name   => $::dart::params::bacula_mon_name,
         mon_passwd => 'NBkHwmNJGplxIlmMaQukmf568KCSuY1eKDGINZnLxyTw',
     }
 
-    include 'dart::abstract::pycharm::professional'
-    include 'dart::mdct_dev12::filesystem'
-    include 'dart::mdct_dev12::libvirt'
-    include 'dart::mdct_dev12::network'
-    include 'dart::mdct_dev12::profile'
-    include 'dart::subsys::mock'
-    include 'dart::subsys::yum_cron'
+    include '::dart::abstract::pycharm::professional'
+    include '::dart::mdct_dev12::filesystem'
+    include '::dart::mdct_dev12::libvirt'
+    include '::dart::mdct_dev12::network'
+    include '::dart::mdct_dev12::profile'
+    include '::dart::subsys::mock'
+    include '::dart::subsys::sigul::params'
+    include '::dart::subsys::yum_cron'
 
-    class { 'koji::cli':
+    class { '::koji::cli':
         hub       => 'http://mdct-koji.dartcontainer.com/kojihub',
         web       => 'http://mdct-koji.dartcontainer.com/koji',
         downloads => 'http://mdct-koji.dartcontainer.com/kojifiles',
@@ -62,7 +63,7 @@ class dart::mdct_dev12 inherits dart::abstract::workstation_node {
         model       => 'foomatic:Dell-3100cn-pxlcolor.ppd',
     }
 
-    class { 'selinux':
+    class { '::selinux':
         mode => 'enforcing',
     }
 
