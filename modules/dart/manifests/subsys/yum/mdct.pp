7ec4ba1db10f318df85d312a0be787b123828867
John Florian <john_florian@dart.biz>
2015-05-12 14:26:43 -0400

    Change - disable Class[dart::subsys::yum::mdct]
    The "mdct" yum repository has been obsoleted by the new "dart"
    repository for hosts running Fedora 20 or newer.  The "mdct"
    repositories are now static and will remain "as is" indefinitely.
    
    Yum updates provide a natural migration path.  This change is necessary
    to prevent Puppet from attempting to undo such an upgrade by
    reinstalling the obsoleted "fedora-mdct-release" package that was once
    necessary for Fedora 20/21 systems.

--- modules/dart/manifests/subsys/yum/mdct.pp
+++ modules/dart/manifests/subsys/yum/mdct.pp
@@ -4,6 +4,11 @@
 #
 # Manages the Yum repository for MDCT software on a Dart host.
 #
+# This repository has been obsoleted by the Dart repository.  All hosts
+# running Fedora 20 and newer should now use that instead.  The MDCT
+# repository remains static "as is" for legacy systems running Fedora 19 or
+# older.
+#
 # === Parameters
 #
 # ==== Required
@@ -22,7 +27,7 @@ class dart::subsys::yum::mdct {
     # Rawhide systems begin life as the latest stable release and should be
     # fully puppetized prior to upgrading to rawhide.  Once there, they have
     # no need for this section, which would only throw errors anyway.
-    if $::operatingsystemrelease != 'Rawhide' {
+    if $::operatingsystemrelease != 'Rawhide' and $::operatingsystemrelease <= 19 {
 
         $pkg_release = $::operatingsystemrelease ? {
             '13'    => '13-1.dcc.noarch',
@@ -32,8 +37,6 @@ class dart::subsys::yum::mdct {
             '17'    => '17-1.dcc.noarch',
             '18'    => '18-2.fc18.noarch',
             '19'    => '19-2.fc19.noarch',
-            '20'    => '20-2.fc20.noarch',
-            '21'    => '21-1.fc21.noarch',
         }
 
         ::yum::repo { 'mdct':
