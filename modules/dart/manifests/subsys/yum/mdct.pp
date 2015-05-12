dfada6794c31b1214ec0d5b3f74a25d2dc8e5510
John Florian <john_florian@dart.biz>
2015-05-12 14:18:08 -0400

    Janitorial - puppet-lint compliance
    - use fully qualified namespace references
    - use Rdoc header format

--- modules/dart/manifests/subsys/yum/mdct.pp
+++ modules/dart/manifests/subsys/yum/mdct.pp
@@ -1,8 +1,23 @@
 # modules/dart/manifests/subsys/yum/mdct.pp
+#
+# == Class: dart::subsys::yum::mdct
+#
+# Manages the Yum repository for MDCT software on a Dart host.
+#
+# === Parameters
+#
+# ==== Required
+#
+# ==== Optional
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
 
 class dart::subsys::yum::mdct {
 
-    include 'dart::subsys::yum::params'
+    include '::dart::subsys::yum::params'
 
     # Rawhide systems begin life as the latest stable release and should be
     # fully puppetized prior to upgrading to rawhide.  Once there, they have
@@ -21,7 +36,7 @@ class dart::subsys::yum::mdct {
             '21'    => '21-1.fc21.noarch',
         }
 
-        yum::repo {'mdct':
+        ::yum::repo { 'mdct':
             server_uri  => "${::dart::subsys::yum::params::fedora_repo_uri}/mdct/${::operatingsystemrelease}/${::architecture}",
             pkg_name    => 'fedora-mdct-release',
             pkg_release => $pkg_release,
