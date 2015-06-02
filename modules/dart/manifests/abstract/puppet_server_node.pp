9e938a02c1f7f501afc64a6e763de3de6805cd34
John Florian <john_florian@dart.biz>
2015-06-02 09:46:50 -0400

    Janitorial - puppet-lint compliance
    - use absolute namespace references
    - Rdoc class documentation
    - puppet-lint compliant formatting

--- modules/dart/manifests/abstract/puppet_server_node.pp
+++ modules/dart/manifests/abstract/puppet_server_node.pp
@@ -1,31 +1,46 @@
 # modules/dart/manifests/abstract/puppet_server_node.pp
+#
+# == Class: dart::abstract::puppet_server_node
+#
+# Manages a Dart host as a Puppet Server node.
+#
+# === Parameters
+#
+# ==== Required
+#
+# ==== Optional
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
 
-class dart::abstract::puppet_server_node inherits dart::abstract::guarded_server_node {
 
-    include 'dart::abstract::packages::developer'
-    include 'dart::subsys::autofs::common'
+class dart::abstract::puppet_server_node inherits ::dart::abstract::guarded_server_node {
 
-    class { 'puppet::client':
+    include '::dart::abstract::packages::developer'
+    include '::dart::subsys::autofs::common'
+
+    class { '::puppet::client':
     }
 
-    class { 'puppet::server':
-        use_passenger   => false,
-        cert_name       => "$fqdn",
+    class { '::puppet::server':
+        use_passenger => false,
+        cert_name     => $::fqdn,
     }
 
-    class { 'puppet::tools':
+    class { '::puppet::tools':
         cron_cleanup => 'puppet:///modules/dart/puppet/tools/puppet-report-cleanup.cron',
         tools_conf   => 'puppet:///modules/dart/puppet/tools/puppet-tools.conf',
     }
 
-    include 'dart::subsys::yum_cron'
+    include '::dart::subsys::yum_cron'
 
-    sendmail::alias { 'puppet':
-        recipient   => 'root',
+    ::sendmail::alias { 'puppet':
+        recipient => 'root',
     }
 
-    sendmail::alias { 'root':
-        recipient   => 'john.florian@dart.biz',
+    ::sendmail::alias { 'root':
+        recipient => 'john.florian@dart.biz',
     }
 
 }
