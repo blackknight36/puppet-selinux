dfc6b0df548e47e6d3274175280994fe7871a48b
John Florian <john_florian@dart.biz>
2015-06-02 11:40:06 -0400

    New - "hiera" module

---
+++ modules/hiera/manifests/init.pp
@@ -0,0 +1,51 @@
+# modules/hiera/manifests/init.pp
+#
+# == Class: hiera
+#
+# Manages Hiera on a host.
+#
+# === Parameters
+#
+# ==== Required
+#
+# ==== Optional
+#
+# [*content*]
+#   Literal content for the hiera configuration file.  If neither "content"
+#   nor "source" is given, the content of the file will be left unmanaged.
+#
+# [*source*]
+#   URI of the hiera configuration file content.  If neither "content" nor
+#   "source" is given, the content of the file will be left unmanaged.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+#   John Florian <jflorian@doubledog.org>
+
+
+class hiera (
+        $content=undef,
+        $source=undef,
+    ) inherits ::hiera::params {
+
+    package { $::hiera::params::packages:
+        ensure => installed,
+    }
+
+    File {
+        owner     => 'root',
+        group     => 'root',
+        mode      => '0644',
+        seluser   => 'system_u',
+        selrole   => 'object_r',
+        seltype   => 'etc_t',
+        subscribe => Package[$::hiera::params::packages],
+    }
+
+    file { '/etc/hiera.yaml':
+        content => $content,
+        source  => $source,
+    }
+
+}
---
+++ modules/hiera/manifests/params.pp
@@ -0,0 +1,28 @@
+# modules/hiera/manifests/params.pp
+#
+# == Class: hiera::params
+#
+# Parameters for the hiera puppet module.
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+#   John Florian <jflorian@doubledog.org>
+
+
+class hiera::params {
+
+    case $::operatingsystem {
+        Fedora: {
+
+            $packages = 'hiera'
+
+        }
+
+        default: {
+            fail ("${title}: operating system '${::operatingsystem}' is not supported")
+        }
+
+    }
+
+}
