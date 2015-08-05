7cd187c573040fb195c5e8b6de19a8c8d5d7aea3
John Florian <john_florian@dart.biz>
2015-08-05 11:28:51 -0400

    New - manage network/NIC on Daniel Brown's workstation

--- modules/dart/manifests/mdct_dev24.pp
+++ modules/dart/manifests/mdct_dev24.pp
@@ -9,6 +9,22 @@
 
 class dart::mdct_dev24 inherits dart::abstract::workstation_node {
 
+    class { '::network':
+        service      => 'nm',
+        domain       => $dart::params::dns_domain,
+        name_servers => $dart::params::dns_servers,
+    }
+
+    network::interface {
+        'eno1':
+            template   => 'static',
+            ip_address => '10.1.250.186',
+            netmask    => '255.255.0.0',
+            gateway    => '10.1.0.25',
+            stp        => 'no',
+            ;
+    }
+
     include '::dart::abstract::idea'
     include '::dart::abstract::pycharm::community'
     include '::dart::subsys::koji::autofs'
