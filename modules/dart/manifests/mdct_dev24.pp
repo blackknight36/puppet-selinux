3225eed30076cce4ff4acd972850c279fea666ac
Daniel Brown <daniel.brown@dart.biz>
2015-10-19 08:44:34 -0400

    Made changes to dev24 for temporary move to new area

--- modules/dart/manifests/mdct_dev24.pp
+++ modules/dart/manifests/mdct_dev24.pp
@@ -18,9 +18,12 @@ class dart::mdct_dev24 inherits dart::abstract::workstation_node {
     network::interface {
         'br0':
             template   => 'static-bridge',
-            ip_address => '10.1.250.186',
-            netmask    => '255.255.0.0',
-            gateway    => '10.1.0.25',
+            ip_address => '10.209.44.24',
+            netmask    => '255.255.252.0',
+            gateway    => '10.209.47.254',
+            #ip_address => '10.1.250.186',
+            #netmask    => '255.255.0.0',
+            #gateway    => '10.1.0.25',
             stp        => 'no',
             mac_address => '0e:00:0d:85:70:15',
             ;
