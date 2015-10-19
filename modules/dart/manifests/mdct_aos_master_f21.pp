e9f77c891544530cd3d0fd0bedffe8a68fee29dc
John Florian <john_florian@dart.biz>
2015-10-19 08:57:04 -0400

    New - host node for AOS Master on Fedora 21
    This host is nearly set to obsolete mdct-aos-master-f20.  Functionally
    it's ready but the DNS CNAME must change yet and a final DB migration
    must also occur.

---
+++ modules/dart/manifests/mdct_aos_master_f21.pp
@@ -0,0 +1,44 @@
+# modules/dart/manifests/mdct_aos_master_f21.pp
+#
+# == Class: dart::mdct_aos_master_f21
+#
+# Configures the AOS Master host.
+#
+# === Parameters
+#
+# NONE
+#
+# === Authors
+#
+#   John Florian <john.florian@dart.biz>
+
+
+class dart::mdct_aos_master_f21 {
+
+    class { '::network':
+        service      => 'nm',
+        domain       => $::dart::params::dns_domain,
+        name_servers => $::dart::params::dns_servers,
+    }
+
+    ::network::interface { 'eth0':
+        template   => 'static',
+        ip_address => '10.1.192.128',
+        netmask    => '255.255.255.0',
+        gateway    => '10.1.0.25',
+    }
+
+    class { '::jaf_bacula::client':
+        dir_name   => $dart::params::bacula_dir_name,
+        dir_passwd => 'hobe9yveB940mWreqoVzfTMIXfWurWi5ROughoJw7A39',
+        mon_name   => $dart::params::bacula_mon_name,
+        mon_passwd => 'zdJxxdFhes9YxlJNhAHFOaSbneIy9N3FmlzTkA1wdowU',
+    }
+
+    class { '::dart::abstract::aos_master_node':
+        django_user  => 'django',
+        django_group => 'django',
+        python_ver   => '3.4',
+    }
+
+}
