5befb92ebea2d9ca84b59955b902a9b91f5e15e0
Collin Baker <collin.baker@dart.biz>
2015-10-19 08:42:06 -0400

    IP address changover for my workstation

--- modules/dart/manifests/mdct_dev17.pp
+++ modules/dart/manifests/mdct_dev17.pp
@@ -15,4 +15,18 @@ class dart::mdct_dev17 inherits dart::abstract::workstation_node {
         'tomcat': port => '8080';
     }
 
+class { 'network':
+        service         => 'nm',
+        domain          => $dart::params::dns_domain,
+        name_servers    => $dart::params::dns_servers,
+    }
+
+network::interface { 'em1':
+        template    => 'static',
+        ip_address  => '10.209.44.17',
+        netmask     => '255.255.252.0',
+        gateway     => '10.209.47.254',
+        stp         => 'no',
+    }
+
 }
