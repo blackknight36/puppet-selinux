ae1b1e1a65ba78d1678c1935955782e7319372dc
John Florian <john_florian@dart.biz>
2015-10-27 14:43:54 -0400

    Drop - management for mdct-repoview
    These repositories are obsolete having been fully replaced by their
    "dart" counterparts.

--- modules/dart/manifests/subsys/koji/repoview.pp
+++ modules/dart/manifests/subsys/koji/repoview.pp
@@ -30,18 +30,11 @@ class dart::subsys::koji::repoview {
             source => 'puppet:///modules/dart/koji/dart-repoview',
             ;
 
-        '/usr/libexec/mdct-repoview':
-            source => 'puppet:///modules/dart/koji/mdct-repoview',
-            ;
-
         '/var/log/dart-repoview':
             ensure => 'directory',
             mode   => '0775',
             ;
     } ->
-
-    # We don't need a cron job for mdct-repoview any more since that content
-    # is all static "as is" now.
 
     ::cron::job { 'dart-repoview':
         command => 'nice ionice -c 3 /usr/libexec/dart-repoview 22 21 20',
