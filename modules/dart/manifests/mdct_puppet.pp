3fe1bf875458278a4494338cbbb9c77401012f73
John Florian <john_florian@dart.biz>
2015-12-17 08:47:35 -0500

    Change - move mdct-puppet-f20 into VLAN 237
    This revision represents the final state though there existed several
    transitional states not reflected here which:
    
        - originally had this network interface configured as eth1 while
          eth0 remained on the legacy VLAN 1 network so that both networks
          could be serviced.
        - provided source-based policy routing so that packets destined for
          the default gateway (such as to hosts not on VLAN 237) would
          return to this host via the proper interface to avoid split brain
          routing issues.  This was needed because the default gateway would
          be either correct pre- or post-DNS changes and the timing of the
          DNS changes could not be known, especially when the TTL of the RR
          is considered.

--- modules/dart/manifests/mdct_puppet.pp
+++ modules/dart/manifests/mdct_puppet.pp
@@ -8,6 +8,16 @@
 
 class dart::mdct_puppet inherits dart::abstract::puppet_server_node {
 
+    include '::network'
+
+    ::network::interface { 'eth0':
+        template   => 'static',
+        ip_address => '10.201.64.41',
+        netmask    => '255.255.252.0',
+        gateway    => '10.201.67.254',
+        stp        => 'no',
+    }
+
     class { 'jaf_bacula::client':
         dir_name   => $dart::params::bacula_dir_name,
         dir_passwd => 'WnB1NCb0T5WEcac5OtHdX47fi7vFreLMicbbt9jsizuC',
