f5e8f9115f418b5cd4e33fd278fda386e275ead6
John Florian <john_florian@dart.biz>
2015-12-29 14:19:12 -0500

    New - PyCharm Professional 5.0.3 stable release

--- modules/dart/manifests/abstract/pycharm/professional.pp
+++ modules/dart/manifests/abstract/pycharm/professional.pp
@@ -24,19 +24,19 @@ class dart::abstract::pycharm::professional {
 
     ::jetbrains::pycharm::release {
 
+        'pycharm-professional-5.0.3':
+            build   => '5.0.3',
+            edition => 'professional',
+            ;
+
         'pycharm-professional-5.0.2':
             build   => '5.0.2',
             edition => 'professional',
             ;
 
         'pycharm-professional-5.0.1':
-            build   => '5.0.1',
-            edition => 'professional',
-            ;
-
-        'pycharm-professional-5.0':
             ensure  => 'absent',
-            build   => '5.0',
+            build   => '5.0.1',
             edition => 'professional',
             ;
 
