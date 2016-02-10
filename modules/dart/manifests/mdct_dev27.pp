279eec2e6714d64004007be5afcdbc545f17d404
John Florian <john_florian@dart.biz>
2016-02-10 10:43:37 -0500

    New - host node mdct-dev27 for Michael Watters

---
+++ modules/dart/manifests/mdct_dev27.pp
@@ -0,0 +1,10 @@
+# modules/dart/manifests/mdct_dev27.pp
+#
+# Synopsis:
+#       DevOps Workstation
+#
+# Contact:
+#       Michael Watters
+
+class dart::mdct_dev27 inherits dart::abstract::workstation_node {
+}
