928cf82417b2e096502e701da44d9cec6f3ac152
John Florian <john_florian@dart.biz>
2016-02-19 13:48:46 -0500

    New - manage filesystem /exports mount point
    This is our conventional location for bind mounting filesystems to be
    exported via NFS.

--- modules/dart/manifests/subsys/filesystem.pp
+++ modules/dart/manifests/subsys/filesystem.pp
@@ -15,14 +15,38 @@
 
 class dart::subsys::filesystem {
 
-    file { '/opt':
+    File {
         ensure  => directory,
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
         seluser => 'system_u',
         selrole => 'object_r',
-        seltype => 'usr_t',
+    }
+
+    # $exports is the standard location exported file systems that are to be
+    # shared via NFS.  Typically those file systems would be bind-mounted here
+    # from their canonical mount point in $storage.
+    $exports = '/exports'
+
+    # $opt is our standard location for packaged software not available via
+    # rpm.  This directory is expected to maintain the following hierarchy:
+    #   $opt/
+    #       vendor1
+    #           product1_from_vendor1
+    #           product2_from_vendor1
+    #       vendor2
+    #           product1_from_vendor2
+    #           product2_from_vendor2
+    $opt = '/opt'
+
+    file {
+        $opt:
+            seltype => 'usr_t',
+            ;
+        $exports:
+            seltype => 'nfs_t',
+            ;
     }
 
 }
