6f0490943156facb8d19dc9f8cf7d864a8f0014d
Michael Watters <michael.watters@dart.biz>
2016-03-14 10:12:18 -0400

    New - Add mdct_dev27_puppetdb host.
    puppetdb is required to store exported resources and can also be used
    as a reporting backend for applications such as puppet explorer and
    puppetboard.

---
+++ environments/production/modules/dart/manifests/mdct_dev27_puppetdb.pp
@@ -0,0 +1,19 @@
+# modules/dart/manifests/mdct_dev27_puppetdb.pp
+#
+# Synopsis:
+#       puppetdb server
+#
+# Contact:
+#       Michael Watters
+
+class dart::mdct_dev27_puppetdb inherits dart::abstract::guarded_server_node {
+
+    include 'puppet::database'
+
+    include 'collectd::client'
+
+    package { ['most', 'mc', 'screen']:
+        ensure => latest,
+    }
+
+}
