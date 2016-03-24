6b7493ed2c3922d7873d9c9a417fc5588d3ff2c0
Michael Watters <michael.watters@dart.biz>
2016-03-24 14:30:54 -0400

    Change - Avoid installing rpm-build-tools on CentOS
    There is currently no rpm-build-tools package available for CentOS
    which causes the puppet agent to fail when puppet attempts to install
    the package.  This update causes rpm_build_tools to do nothing if the
    operating system installed is not Fedora.

--- environments/production/modules/rpm_build_tools/manifests/init.pp
+++ environments/production/modules/rpm_build_tools/manifests/init.pp
@@ -2,41 +2,43 @@
 
 class rpm_build_tools {
 
-    package { "rpm-build-tools":
-        ensure  => latest,
-    }
+    if $::operatingsystem == 'Fedora' {
 
-    file { "/etc/rpm-build-tools/git.conf":
-        group   => "root",
-        mode    => "0644",
-        owner   => "root",
-        require => Package["rpm-build-tools"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
-        source  => "puppet:///modules/rpm_build_tools/git.conf",
-    }
+        package { "rpm-build-tools":
+            ensure  => latest,
+        }
 
-    file { "/etc/rpm-build-tools/rpmbuild.conf":
-        group   => "root",
-        mode    => "0644",
-        owner   => "root",
-        require => Package["rpm-build-tools"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
-        source  => "puppet:///modules/rpm_build_tools/rpmbuild.conf",
-    }
+        file { "/etc/rpm-build-tools/git.conf":
+            group   => "root",
+            mode    => "0644",
+            owner   => "root",
+            require => Package["rpm-build-tools"],
+            seluser => "system_u",
+            selrole => "object_r",
+            seltype => "etc_t",
+            source  => "puppet:///modules/rpm_build_tools/git.conf",
+        }
 
-    file { "/etc/rpm-build-tools/yum.conf":
-        group   => "root",
-        mode    => "0644",
-        owner   => "root",
-        require => Package["rpm-build-tools"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
-        source  => "puppet:///modules/rpm_build_tools/yum.conf",
-    }
+        file { "/etc/rpm-build-tools/rpmbuild.conf":
+            group   => "root",
+            mode    => "0644",
+            owner   => "root",
+            require => Package["rpm-build-tools"],
+            seluser => "system_u",
+            selrole => "object_r",
+            seltype => "etc_t",
+            source  => "puppet:///modules/rpm_build_tools/rpmbuild.conf",
+        }
 
+        file { "/etc/rpm-build-tools/yum.conf":
+            group   => "root",
+            mode    => "0644",
+            owner   => "root",
+            require => Package["rpm-build-tools"],
+            seluser => "system_u",
+            selrole => "object_r",
+            seltype => "etc_t",
+            source  => "puppet:///modules/rpm_build_tools/yum.conf",
+        }
+    }
 }
