2f2bc79ac55074a91f47756bcf0d756e2147b1a8
Michael Watters <michael.watters@dart.biz>
2016-03-29 08:04:43 -0400

    Bug - Update yumrepo parameters for dart::subsys::yum::rpmfusion
    Due to a bug in how yum repo resources are evaluated puppet may
    truncate repo files if all repo parameters are not specified.  This
    bug affects all repos managed using the yumrepo resource type.
    
    See https://tickets.puppetlabs.com/browse/PUP-723 for more details.

--- environments/production/modules/dart/manifests/subsys/yum/rpmfusion.pp
+++ environments/production/modules/dart/manifests/subsys/yum/rpmfusion.pp
@@ -25,13 +25,21 @@ class dart::subsys::yum::rpmfusion {
 
     # Fedora 23 needs to have the development repos enabled for rpmfusion packages
     if $::operatingsystem == 'Fedora' and $::operatingsystemrelease == '23' {
+
         yumrepo { 'local-rpmfusion-free-development':
-            enabled => 1,
-            require => Yum::Repo['rpmfusion-free'],
+            descr   => 'MDCT mirror of RPM Fusion for Fedora $releasever Free - Development',
+            enabled => '1',
+            baseurl => 'http://mdct-00fs.dartcontainer.com/ftp/pub/fedora/rpmfusion/free/fedora/development/$releasever/$basearch/os/',
+            gpgcheck => '1',
+            gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmfusion-free-fedora-$releasever',
         }
+
         yumrepo { 'local-rpmfusion-nonfree-development':
-            enabled => 1,
-            require => Yum::Repo['rpmfusion-nonfree'],
+            descr   => 'MDCT mirror of RPM Fusion for Fedora $releasever - Nonfree - Development',
+            enabled => '1',
+            baseurl => 'http://mdct-00fs.dartcontainer.com/ftp/pub/fedora/rpmfusion/nonfree/fedora/development/$releasever/$basearch/os/',
+            gpgcheck => '1',
+            gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmfusion-nonfree-fedora-$releasever',
         }
 
         yumrepo { ['local-rpmfusion-free', 'local-rpmfusion-free-updates']:
