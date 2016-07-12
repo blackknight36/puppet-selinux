0778adbfdaf8b2f7f8fea7ff41363ae1b9d3cc51
Michael Watters <michael.watters@dart.biz>
2016-07-12 17:15:36 -0400

    Change - Include selinux::params in selinux::module manifest
    The selinux::params class needs to be included in order to access
    variables defined within that class properly.

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,4 +1,4 @@
-# Copyright 2016 Dart Container 
+# Copyright 2016 Dart Container
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -49,6 +49,8 @@ define selinux::module(
   $modules_dir = undef,
   $ignore = undef,
 ) {
+
+  include 'selinux::params'
 
   $ensures = [ 'present', 'enabled', 'disabled', 'absent' ]
 
